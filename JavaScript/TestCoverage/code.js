eb45cc41f2bcc9a10b1190180abcbab6a3451e2b
anton <anton.al.ivanov@gmail.com>
2014-07-07 23:59:51 +0300

    Minor re-factoring

--- JavaScript/TestCoverage/code.js
+++ JavaScript/TestCoverage/code.js
@@ -1,33 +1,33 @@
 (function(host) {
 
-  var info = {
+  var dictionary = {
     "key1": "value1",
     "key2": "value2",
     "key3": "value3"
   };
 
   //Broken implementation
-  function getInfoBroken(keys) {
+  function getValuesBroken(keys) {
     return ["value1", "value2", "value3"];
   }
 
-  function getInfo(keys) {
+  function getValues(keys) {
     return keys.map(function(key) {
-      return info[key];
+      return dictionary[key];
     });
   }
 
   //Expects malformed input
-  function getInfoCautious(keys) {
+  function getValuesCautious(keys) {
     return keys ? keys.map(function(key) {
-      return info[key];
+      return dictionary[key];
     }) : [];
   }
 
   var exported = {
-    getInfo: getInfo,
-    getInfoBroken: getInfoBroken,
-    getInfoCautious: getInfoCautious
+    getValues: getValues,
+    getValuesBroken: getValuesBroken,
+    getValuesCautious: getValuesCautious
   };
   if (module && module.exports) {
     module.exports = exported;
--- JavaScript/TestCoverage/code.spec.js
+++ JavaScript/TestCoverage/code.spec.js
@@ -1,31 +1,31 @@
 var code = require("./code");
-var getInfo = code.getInfo;
-var getInfoBroken = code.getInfoBroken;
-var getInfoCautious = code.getInfoCautious;
+var getValues = code.getValues;
+var getValuesBroken = code.getValuesBroken;
+var getValuesCautious = code.getValuesCautious;
 var sets = require("./generator").sets;
 
 //Full line coverage, tests pass, implementation is largely correct
-describe("getInfo - full line coverage", function() {
+describe("getValues - full line coverage", function() {
 
   it("should provide values for requested keys", function() {
-    expect(getInfo(["key1", "key2", "key3"])).toEqual(["value1", "value2", "value3"]);
+    expect(getValues(["key1", "key2", "key3"])).toEqual(["value1", "value2", "value3"]);
   });
 });
 
 //Full line coverage, tests pass, but the implementation is broken
-describe("getInfoBroken - full line coverage", function() {
+describe("getValuesBroken - full line coverage", function() {
 
   it("should provide values for requested keys", function() {
-    expect(getInfoBroken(["key1", "key2", "key3"])).toEqual(["value1", "value2", "value3"]);
+    expect(getValuesBroken(["key1", "key2", "key3"])).toEqual(["value1", "value2", "value3"]);
   });
 
   //Fails! "Expected [ 'value1', 'value2', 'value3' ] to equal [ 'value1' ]."
   xit("should provide values for requested single key", function() {
-    expect(getInfoBroken(["key1"])).toEqual(["value1"]);
+    expect(getValuesBroken(["key1"])).toEqual(["value1"]);
   });
 });
 
-describe("getInfo - full 'happy-path' scenario coverage", function() {
+describe("getValues - full 'happy-path' scenario coverage", function() {
 
   sets(["key1", "key2", "key3"]).forEach(function(testInput) {
     var testCaseName = "should provide values for '" + testInput.join(",") + "'";
@@ -34,17 +34,17 @@ describe("getInfo - full 'happy-path' scenario coverage", function() {
       var expectedOutput = testInput.map(function(key) {
         return key.replace("key", "value");
       });
-      expect(getInfo(testInput)).toEqual(expectedOutput);
+      expect(getValues(testInput)).toEqual(expectedOutput);
     });
   });
 
   //Fails! "TypeError: Cannot call method 'map' of null"
   xit("should handle invalid input properly", function() {
-    expect(getInfo(null)).toEqual([]);
+    expect(getValues(null)).toEqual([]);
   });
 });
 
-describe("getInfoCautious - full 'happy-path' scenario coverage plus some corner cases, defensive style", function() {
+describe("getValuesCautious - full 'happy-path' scenario coverage plus some corner cases, defensive style", function() {
 
   sets(["key1", "key2", "key3"]).forEach(function(testInput) {
     var testCaseName = "should provide values for '" + testInput.join(",") + "'";
@@ -53,15 +53,15 @@ describe("getInfoCautious - full 'happy-path' scenario coverage plus some corner
       var expectedOutput = testInput.map(function(key) {
         return key.replace("key", "value");
       });
-      expect(getInfoCautious(testInput)).toEqual(expectedOutput);
+      expect(getValuesCautious(testInput)).toEqual(expectedOutput);
     });
   });
 
   it("should handle invalid input properly", function() {
-    expect(getInfoCautious(null)).toEqual([]);
+    expect(getValuesCautious(null)).toEqual([]);
   });
 
   it("should return no value when unknown key is used", function() {
-    expect(getInfoCautious(["key1", "unknown", "key3"])).toEqual(["value1", undefined, "value3"]);
+    expect(getValuesCautious(["key1", "unknown", "key3"])).toEqual(["value1", undefined, "value3"]);
   });
 });
