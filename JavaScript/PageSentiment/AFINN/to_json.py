48503083950002215d0ce158a606e27de4738f4f
anton <anton.al.ivanov@gmail.com>
2014-08-17 23:01:43 +0300

    Generated the JSON version of word sentiment data

---
+++ JavaScript/PageSentiment/AFINN/to_json.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+sentiment_file_name = os.path.join('.', 'AFINN', 'AFINN-111.txt')
+sentiment_json_file_name = os.path.join('.', 'afinn-111.json')
+
+sentiments = {}
+
+with open(sentiment_file_name) as f:
+    for line in f:
+        word, sentiment = line.strip().split('\t')
+        sentiments[word] = int(sentiment)
+
+with open(sentiment_json_file_name, 'w') as f:
+  json.dump(sentiments, f, sort_keys=True, indent=2)
