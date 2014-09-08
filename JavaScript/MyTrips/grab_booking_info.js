a8e89a8385570fe04e32c2fb0f1eec2b6d388aac
anton <anton.al.ivanov@gmail.com>
2014-09-08 23:06:54 +0200

    Added bower.json, using Promises to untangle the callback spaghetti in PhantomJS code, loading the personal e-mail messages.

--- JavaScript/MyTrips/grab_booking_info.js
+++ JavaScript/MyTrips/grab_booking_info.js
@@ -1,8 +1,10 @@
+phantom.injectJs('bower_components/es6-promise/promise.min.js');
+
 /*
  * Script that grabs Booking.com booking info from Gmail.
  */
 var CHECK_INTERVAL_MS = 1000;
-var MAX_WAIT_TIME_MS = 10000;
+var MAX_WAIT_TIME_MS = 5000;
 
 var email = "<username>@gmail.com";
 var password = "<password>";
@@ -14,18 +16,18 @@ page.viewportSize = {
   height: 768
 };
 
-function whenElementAvailable(page, selector, callback) {
+function whenElementAvailable(page, cssSelector, callback, errback) {
 
   var timeWaited = 0;
 
   function check() {
     if (timeWaited > MAX_WAIT_TIME_MS) {
-      throw new Error("Timed out waiting for '" + selector + "'");
+      errback && errback(new Error("Timed out waiting for '" + cssSelector + "'"));
     }
 
-    var isAvailable = page.evaluate(function(selector) {
-      return document.querySelector(selector) != null;
-    }, selector);
+    var isAvailable = page.evaluate(function(cssSelector) {
+      return document.querySelector(cssSelector) != null;
+    }, cssSelector);
 
     if (isAvailable) {
       callback();
@@ -38,7 +40,30 @@ function whenElementAvailable(page, selector, callback) {
   check();
 }
 
-page.open('https://accounts.google.com', function(status) {
+//TODO: Make into methods on the created page
+//TODO: Extract as a separate library, publish it to Bower?
+
+function open(page, url) {
+
+  //TODO: Error handling
+  return new Promise(function(resolve, reject) {
+    page.open(url, function(status) {
+      resolve(status);
+    });
+  });
+}
+
+function waitFor(page, cssSelector) {
+   return new Promise(function(resolve, reject) {
+     whenElementAvailable(page, cssSelector, function() {
+       resolve(cssSelector);
+     }, function(error) {
+       reject(error);
+     });
+   });
+};
+
+open(page, 'https://accounts.google.com').then(function() {
   page.evaluate(function(email, password) {
     var emailInput = document.querySelector("#Email");
     var passwordInput = document.querySelector("#Passwd");
@@ -50,13 +75,16 @@ page.open('https://accounts.google.com', function(status) {
     signInButton.click();
   }, email, password);
 
-  //Wait for login to finish
-  whenElementAvailable(page, "#nav-personalinfo", function() {
-    page.render('mail.png');
-    //TODO: Open the mail page https://mail.google.com
-    //TODO: Search for customer.service@booking.com
-    //TODO: Iterate over all the e-mails that matched and extract the Booking information:
-    //city, address, start date, end date
-    phantom.exit();
-  });
+  return waitFor(page, "#nav-personalinfo");
+}).then(function() {
+  return open(page, 'https://mail.google.com');
+}).then(function() {
+  return waitFor(page, "button[aria-label]");
+}).then(function() {
+  page.render('mail.png');
+  phantom.exit();
 });
+
+//TODO: Search for customer.service@booking.com
+//TODO: Iterate over all the e-mails that matched and extract the Booking information:
+//city, address, start date, end date
