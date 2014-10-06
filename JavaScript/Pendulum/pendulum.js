301885800fbe9487428d354ad55052927c52783f
anton <anton.al.ivanov@gmail.com>
2014-10-06 23:27:34 +0200

    Implemented the Physics motion laws for the pendulum

--- JavaScript/Pendulum/pendulum.js
+++ JavaScript/Pendulum/pendulum.js
@@ -1,36 +1,75 @@
 var canvasWidth = 400;
-var canvasHeight = 400;
-var animationIntervalMs = 100;
+var canvasHeight = 200;
+var animationIntervalMs = 20;
+
+var coordinatesZero = {
+  x: canvasWidth / 2,
+  y: 0
+};
 
 var drawingContext;
 
 /*
  * Physical characteristics.
  */
-var pendulumRadius = 5;
-var l = 100;
-var alpha = -Math.PI / 4;
 
-var coordinatesZero = {
-  x: canvasWidth / 2,
-  y: canvasHeight / 2
-};
+//Radius of the pendulum
+var pendulumRadius = 10;
 
-function computeCoordinates(alpha, l) {
-  return {
-    x: l * Math.sin(alpha),
-    y: l * Math.cos(alpha)
-  };
+//Length of the string
+var l = 150;
+
+//Initial angle of the pendulum
+var alpha = - 3 * Math.PI / 8;
+
+//Resistance coefficient of the liquid
+var beta = 0.003;
+
+//Time is discrete in our physical world, this is the increment between two adjacent time values
+var deltaT = 0.1;
+
+//Mass of the pendulum
+var m = 10;
+
+//Initial value of the radial speed
+var omega = 0;
+
+//Earth gravity acceleration
+var g = 9.81;
+
+/*
+ * Laws of Physics.
+ */
+function updatePhysicalValues() {
+  omega = omega + (-(g * Math.sin(alpha)) / l - (beta * l * omega) / m) * deltaT;
+  alpha = alpha + omega * deltaT;
+}
+
+/*
+ * Drawing.
+ */
+function getDrawingContext() {
+  var display = document.querySelector("#display");
+  var drawingContext = display.getContext("2d");
+
+  display.width = canvasWidth;
+  display.height = canvasHeight;
+  return drawingContext;
 }
 
 function drawPendulum(alpha, l) {
-  var coordinates = computeCoordinates(alpha, l);
+  var coordinates = {
+    x: l * Math.sin(alpha),
+    y: l * Math.cos(alpha)
+  };
+
   var displayCoordinates = {
     x: coordinatesZero.x + coordinates.x,
     y: coordinatesZero.y + coordinates.y
   };
 
   drawingContext.clearRect(0, 0, canvasWidth, canvasHeight);
+  drawingContext.fillStyle = "black";
   drawingContext.beginPath();
   drawingContext.arc(displayCoordinates.x, displayCoordinates.y, pendulumRadius, 0, 2 * Math.PI, false);
   drawingContext.fill();
@@ -39,25 +78,16 @@ function drawPendulum(alpha, l) {
   drawingContext.stroke();
 }
 
-function init() {
-  var display = document.querySelector("#display");
-
-  display.width = canvasWidth;
-  display.height = canvasHeight;
-  drawingContext = display.getContext("2d");
-  drawingContext.fillStyle = "black";
-
-  var alphas = [-Math.PI / 4, -Math.PI / 5, -Math.PI / 6, -Math.PI / 7, -Math.PI / 8];
-  var i = 0;
-
-  var animationInterval = setInterval(function() {
-    if (i < alphas.length) {
-      drawPendulum(alphas[i], l);
-      i++;
-    } else {
-      clearInterval(animationInterval);
-    }
+function runSimulation() {
+  setInterval(function() {
+    drawPendulum(alpha, l);
+    updatePhysicalValues();
   }, animationIntervalMs);
+}
+
+function init() {
+  drawingContext = getDrawingContext();
+  runSimulation();
 }
 
 window.addEventListener("load", init);
