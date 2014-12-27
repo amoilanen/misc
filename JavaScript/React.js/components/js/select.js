7d8d1ad25308703521c149066b37d53132c01368
Anton Ivanov <anton.al.ivanov@gmail.com>
2014-12-27 07:35:13 +0200

    Options are rendered dynamically

--- JavaScript/React.js/components/js/select.js
+++ JavaScript/React.js/components/js/select.js
@@ -1,4 +1,51 @@
+var SelectOption = React.createClass({
+  render: function() {
+    return (
+      <li>{this.props.label}</li>
+    );
+  }
+});
+
+var SelectOptionList = React.createClass({
+  render: function() {
+    var options = this.props.options.map(function(option, index) {
+      return (
+        <SelectOption label={option.label} key={index} />
+      );
+    });
+
+    return (
+      <div className="rc-select--options">
+        <ul>
+          {options}
+        </ul>
+      </div>
+    );
+  }
+});
+
 var Select = React.createClass({
+  getInitialState: function() {
+    return {
+      options: [
+        {
+          label: "Prague"
+        },
+        {
+          label: "Berlin"
+        },
+        {
+          label: "Helsinki"
+        },
+        {
+          label: "Amsterdam"
+        },
+        {
+          label: "Paris"
+        }
+      ]
+    };
+  },
   render: function() {
     return (
       <div className="rc-select">
@@ -6,15 +53,7 @@ var Select = React.createClass({
           <div className="rc-select--input">abcdef</div>
           <div className="rc-select--arrow"></div>
         </div>
-        <div className="rc-select--options">
-          <ul>
-            <li>Prague</li>
-            <li>Berlin</li>
-            <li>Helsinki</li>
-            <li>Amsterdam</li>
-            <li>Paris</li>
-          </ul>
-        </div>
+        <SelectOptionList options={this.state.options} />
       </div>
     );
   }
