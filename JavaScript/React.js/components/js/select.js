c0c434c690ecc9bc43ce76c202b6ad9a517be26e
Anton Ivanov <anton.al.ivanov@gmail.com>
2014-12-28 17:38:22 +0200

    Added active index which is updated on keyboard arrow up and arrow down events

--- JavaScript/React.js/components/js/select.js
+++ JavaScript/React.js/components/js/select.js
@@ -4,8 +4,14 @@ var SelectOption = React.createClass({
     event.stopPropagation();
   },
   render: function() {
-    var className = this.props.selected ? "rc-select--option rc-select--option_selected" : "rc-select--option";
+    var className = "rc-select--option";
 
+    if (this.props.selected) {
+      className = className + " rc-select--option_selected";
+    }
+    if (this.props.active) {
+      className = className + " rc-select--option_active";
+    }
     return (
       <li value={this.props.value} className={className} onClick={this.onSelect}>
         <div className="rs-selection--option-label">{this.props.label}</div>
@@ -19,7 +25,7 @@ var SelectOptionList = React.createClass({
     var self = this;
     var options = this.props.options.map(function(option, index) {
       return (
-        <SelectOption label={option.label} value={option.value} selected={option.selected} key={index} onSelect={self.props.onSelect}/>
+        <SelectOption active={index === self.props.activeIndex} label={option.label} value={option.value} selected={option.selected} key={index} onSelect={self.props.onSelect}/>
       );
     });
 
@@ -39,7 +45,8 @@ var Select = React.createClass({
   },
   getInitialState: function() {
     return {
-      active: false
+      active: false,
+      activeIndex: 3
     };
   },
   toggle: function(active) {
@@ -48,7 +55,10 @@ var Select = React.createClass({
     }
     var focusTrapElement = this.refs.rcSelectFocusTrap.getDOMNode();
 
-    this.setState({active: active});
+    this.setState({
+      active: active,
+      activeIndex: 3
+    });
     if (active) {
       focusTrapElement.focus();
     } else {
@@ -60,9 +70,22 @@ var Select = React.createClass({
   },
   onKeyUp: function(event) {
     var nativeEvent = event.nativeEvent;
+    var activeIndex = this.state.activeIndex;
 
     if (nativeEvent.keyCode === 27) {
       this.toggle(false);
+    } else if (nativeEvent.keyCode === 40) {
+      activeIndex++;
+      if (activeIndex >= this.props.options.length) {
+        activeIndex = 0;
+      }
+      this.setState({activeIndex: activeIndex});
+    } else if (nativeEvent.keyCode === 38) {
+      activeIndex--;
+      if (activeIndex < 0) {
+        activeIndex = this.props.options.length - 1;
+      }
+      this.setState({activeIndex: activeIndex});
     }
   },
   isInsideComponent: function(domElement) {
@@ -99,7 +122,7 @@ var Select = React.createClass({
           <div className="rc-select--input">{selectedOption.label}</div>
           <div className="rc-select--arrow"></div>
         </div>
-        <SelectOptionList options={this.props.options} active={this.state.active} onSelect={this.select}/>
+        <SelectOptionList activeIndex={this.state.activeIndex} options={this.props.options} active={this.state.active} onSelect={this.select}/>
       </div>
     );
   }
