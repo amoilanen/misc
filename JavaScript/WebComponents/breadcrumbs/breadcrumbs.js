d3bcc9e78fbafac2b052df0e64c620f60d841380
Anton Ivanov <anton.al.ivanov@gmail.com>
2015-01-08 22:33:47 +0200

    Stub for the Breadcrumbs component

--- JavaScript/WebComponents/breadcrumbs/breadcrumbs.js
+++ JavaScript/WebComponents/breadcrumbs/breadcrumbs.js
@@ -1,1 +1,17 @@
-console.log('Breadcrumbs loaded...');
+(function() {
+
+  var prototype = Object.create(HTMLElement.prototype);
+
+  prototype.createdCallback = function() {
+    var root = this.createShadowRoot();
+
+    var crumb = document.createElement('div');
+
+    crumb.setAttribute('class', 'crumb');
+
+    root.appendChild(crumb);
+  }
+  document.registerElement('rc-breadcrumbs', {
+    prototype: prototype
+  });
+})();
