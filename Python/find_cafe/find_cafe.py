b5358245e701c8cbe9c96f78ecf39f1574e64515
Anton Ivanov <anton.al.ivanov@gmail.com>
2015-01-17 17:06:58 +0200

    Fetching menu from another cafe

--- Python/find_cafe/find_cafe.py
+++ Python/find_cafe/find_cafe.py
@@ -16,6 +16,10 @@ def format_date(d):
     return '%d\\.%d\\.' % (d.day, d.month)
 
 def get_hima_sali_menu(date):
+    weekday = date.weekday()
+    if (weekday > 4): #Saturday or Sunday
+        return 'Weekend: no menu'
+
     date_label = format_date(date)
 
     response = urllib.request.urlopen(HIMA_SALI_URL)
@@ -27,8 +31,26 @@ def get_hima_sali_menu(date):
     else:
         return dates_menu[-1]
 
+def get_dylan_milk_menu(date):
+    weekday = date.weekday()
+    if (weekday > 4): #Saturday or Sunday
+        return 'Weekend: no menu'
+
+    response = urllib.request.urlopen(DYLAN_MILK_URL)
+    html = response.read().decode(response.headers.get_content_charset())
+
+    dates_menu = re.findall(r'BUFFET:<br />(.*?)<br /> <br />', html, re.MULTILINE | re.DOTALL)
+    if (len(dates_menu) == 0):
+        return 'No menu'
+    else:
+        return dates_menu[0]
+
 today = date.today()
+#today = today + timedelta(days=2)
+
 print('Today %s\n' % today.strftime('%d.%m.%y'))
 
 hima_sali_menu = get_hima_sali_menu(today)
-print('Hima & Sali:\n\n%s' % hima_sali_menu)
+print('\nHima & Sali:\n\n%s' % hima_sali_menu.replace('<br />', '').replace('&amp;', '&').replace('&nbsp;', ''))
+dylan_milk_menu = get_dylan_milk_menu(today)
+print('\nDylan Milk:\n\n%s' % dylan_milk_menu.replace('<br />', '\n'))
