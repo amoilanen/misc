72c62aed77bb6396fe4336cbfc46d53bb7df8df8
Anton Ivanov <anton.al.ivanov@gmail.com>
2015-01-17 17:17:28 +0200

    Re-factoring so that adding more cafes requires less repetitive code

--- Python/find_cafe/find_cafe.py
+++ Python/find_cafe/find_cafe.py
@@ -12,38 +12,27 @@ DYLAN_MILK_URL = 'http://dylan.fi/milk/'
 #Weather service
 #http://yle.fi/saa/suomi/helsinki/pasila/
 
-def format_date(d):
-    return '%d\\.%d\\.' % (d.day, d.month)
+def get_and_find_all(url, regex):
+    response = urllib.request.urlopen(url)
+    html = response.read().decode(response.headers.get_content_charset())
+    return re.findall(regex, html, re.MULTILINE | re.DOTALL)
+
+def find_menu(url, date, regex, index=0):
+    weekday = date.weekday()
+    if (weekday > 4): #Saturday or Sunday
+        return 'Weekend: no menu'
+    found = get_and_find_all(url, regex)
+    if (len(found) == 0):
+        return 'No menu'
+    else:
+        return found[index]
 
 def get_hima_sali_menu(date):
-    weekday = date.weekday()
-    if (weekday > 4): #Saturday or Sunday
-        return 'Weekend: no menu'
-
-    date_label = format_date(date)
-
-    response = urllib.request.urlopen(HIMA_SALI_URL)
-    html = response.read().decode(response.headers.get_content_charset())
-    dates_menu = re.findall(r'%s<br />\n(.*?)\n<br />' % (date_label), html, re.MULTILINE | re.DOTALL)
-
-    if (len(dates_menu) == 0):
-        return 'No menu'
-    else:
-        return dates_menu[-1]
+    date_label = '%d\\.%d\\.' % (date.day, date.month)
+    return find_menu(HIMA_SALI_URL, date, r'%s<br />\n(.*?)\n<br />' % (date_label), -1)
 
 def get_dylan_milk_menu(date):
-    weekday = date.weekday()
-    if (weekday > 4): #Saturday or Sunday
-        return 'Weekend: no menu'
-
-    response = urllib.request.urlopen(DYLAN_MILK_URL)
-    html = response.read().decode(response.headers.get_content_charset())
-
-    dates_menu = re.findall(r'BUFFET:<br />(.*?)<br /> <br />', html, re.MULTILINE | re.DOTALL)
-    if (len(dates_menu) == 0):
-        return 'No menu'
-    else:
-        return dates_menu[0]
+    return find_menu(DYLAN_MILK_URL, date, r'BUFFET:<br />(.*?)<br /> <br />')
 
 today = date.today()
 #today = today + timedelta(days=2)
