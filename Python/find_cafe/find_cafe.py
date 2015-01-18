6ae5127605f43cb725905b326c86c06b9d396da6
Anton Ivanov <anton.al.ivanov@gmail.com>
2015-01-18 18:31:24 +0200

    Using the web service to get the weather data rather than parsin HTML: more robust

--- Python/find_cafe/find_cafe.py
+++ Python/find_cafe/find_cafe.py
@@ -8,11 +8,12 @@ from constants import NEPALESE, CHINESE, HIMA_SALI, DYLAN_MILK
 
 from datetime import date, timedelta
 import urllib.request
+import json
 import re
 
 HIMA_SALI_URL = 'http://www.himasali.com/p/lounaslista.html'
 DYLAN_MILK_URL = 'http://dylan.fi/milk/'
-YLE_WEATHER_URL = 'http://yle.fi/saa/suomi/helsinki/pasila/'
+YLE_WEATHER_FORECAST_URL = 'http://yle.fi/saa/resources/ajax/saa-api/hourly-forecast.action?id=642554'
 
 def get(url):
     response = urllib.request.urlopen(url)
@@ -43,48 +44,39 @@ def parse_weather_value(regex, html):
     value = re.findall(regex, html, re.MULTILINE | re.DOTALL)[0]
     return int(value.replace('&lt;', ''))
 
-WEATHER_REGEXES = {
-    'TEMPERATURE': '"temperature-container".*?class="value">(.*?)</span>',
-    'PRECIPITATION_CHANCE': '"precipitation-container".*?class="value">(.*?)</span>',
-    'PRECIPITATION_AMOUNT': 'precipitation-1h".*?(\d+)\.\d* mm',
-    'WIND': 'wind-icon.*?class="value">(.*?)</span>'
-}
-
 def get_todays_weather():
-    html = get(YLE_WEATHER_URL)
+    forecast = json.loads(get(YLE_WEATHER_FORECAST_URL))['weatherInfos'][0]
     return {
-        TEMPERATURE: parse_weather_value(WEATHER_REGEXES['TEMPERATURE'], html),
-        PRECIPITATION_CHANCE: parse_weather_value(WEATHER_REGEXES['PRECIPITATION_CHANCE'], html),
-        PRECIPITATION_AMOUNT: parse_weather_value(WEATHER_REGEXES['PRECIPITATION_AMOUNT'], html),
-        WIND: parse_weather_value(WEATHER_REGEXES['WIND'], html)
+        TEMPERATURE: forecast['temperature'],
+        PRECIPITATION_CHANCE: forecast['probabilityPrecipitation'],
+        PRECIPITATION_AMOUNT: forecast['precipitation1h'],
+        WIND: forecast['windSpeedMs']
     }
 
 today = date.today()
 today = today + timedelta(days=2)
-
-print('Today %s\n' % today.strftime('%d.%m.%y'))
-
-hima_sali_menu = get_hima_sali_menu(today)
-print('\nHima & Sali:\n\n%s' % hima_sali_menu.replace('<br />', '').replace('&amp;', '&').replace('&nbsp;', ''))
-
-dylan_milk_menu = get_dylan_milk_menu(today)
-print('\nDylan Milk:\n\n%s' % dylan_milk_menu.replace('<br />', '\n'))
-
-weather = get_todays_weather()
-print('\nWeather:\n\n temperature %s C\n chance of precipitation %s percent\n precipitation %s mm\n wind %s m/s' % tuple(weather.values()))
 
 #hima_sali_menu = 'meatballs'
 #dylan_milk_menu = 'fish'
 #weather = {
 #    TEMPERATURE: 2,
 #    PRECIPITATION_CHANCE: 10,
-#    PRECIPITATION_AMOUNT: 2,
+#    PRECIPITATION_AMOUNT: 2.0,
 #    WIND: 5
 #}
 
+print('Today %s\n' % today.strftime('%d.%m.%y'))
+
+hima_sali_menu = get_hima_sali_menu(today)
+print('\nHima & Sali:\n\n%s' % hima_sali_menu.replace('<br />', '').replace('&amp;', '&').replace('&nbsp;', ''))
+dylan_milk_menu = get_dylan_milk_menu(today)
+print('\nDylan Milk:\n\n%s' % dylan_milk_menu.replace('<br />', '\n'))
+
+weather = get_todays_weather()
+print('\nWeather:\n\n temperature %s C\n chance of precipitation %s percent\n precipitation %s mm\n wind %s m/s' % (weather[TEMPERATURE], weather[PRECIPITATION_CHANCE], weather[PRECIPITATION_AMOUNT], weather[WIND]))
+
 #TODO: Read from the local file where previous lunches for the current week are stored
 lunch_history = [HIMA_SALI, DYLAN_MILK]
-
 print('\nLunch history for current week:\n\n %s' % ', '.join(lunch_history))
 
 cafe_menus = {
@@ -95,7 +87,5 @@ cafe_menus = {
 }
 
 clo = ChiefLunchOfficer()
-
 clo.history(lunch_history).weather(weather).menus(cafe_menus)
-
 print('\nRecommendation:\n\n %s' % clo.decide())
