056a7143b84935c5d171bd23a9c772520c609caa
Anton Ivanov <anton.al.ivanov@gmail.com>
2015-01-18 16:50:54 +0200

    Fetching the weather data for today

--- Python/find_cafe/find_cafe.py
+++ Python/find_cafe/find_cafe.py
@@ -8,13 +8,14 @@ import re
 
 HIMA_SALI_URL = 'http://www.himasali.com/p/lounaslista.html'
 DYLAN_MILK_URL = 'http://dylan.fi/milk/'
+YLE_WEATHER_URL = 'http://yle.fi/saa/suomi/helsinki/pasila/'
 
-#Weather service
-#http://yle.fi/saa/suomi/helsinki/pasila/
+def get(url):
+    response = urllib.request.urlopen(url)
+    return response.read().decode(response.headers.get_content_charset())
 
 def get_and_find_all(url, regex):
-    response = urllib.request.urlopen(url)
-    html = response.read().decode(response.headers.get_content_charset())
+    html = get(url)
     return re.findall(regex, html, re.MULTILINE | re.DOTALL)
 
 def find_menu(url, date, regex, index=0):
@@ -34,6 +35,24 @@ def get_hima_sali_menu(date):
 def get_dylan_milk_menu(date):
     return find_menu(DYLAN_MILK_URL, date, r'BUFFET:<br />(.*?)<br /> <br />')
 
+def parse_weather_value(regex, html):
+    value = re.findall(regex, html, re.MULTILINE | re.DOTALL)[0]
+    return int(value.replace('&lt;', ''))
+
+WEATHER_REGEXES = {
+    'TEMPERATURE': '"temperature-container".*?class="value">(.*?)</span>',
+    'PRECIPITATION': '"precipitation-container".*?class="value">(.*?)</span>',
+    'WIND': 'wind-icon.*?class="value">(.*?)</span>'
+}
+
+def get_todays_weather():
+    html = get(YLE_WEATHER_URL)
+    return {
+        'temperature': parse_weather_value(WEATHER_REGEXES['TEMPERATURE'], html),
+        'precipitation': parse_weather_value(WEATHER_REGEXES['PRECIPITATION'], html),
+        'wind': parse_weather_value(WEATHER_REGEXES['WIND'], html)
+    }
+
 today = date.today()
 #today = today + timedelta(days=2)
 
@@ -41,5 +60,9 @@ print('Today %s\n' % today.strftime('%d.%m.%y'))
 
 hima_sali_menu = get_hima_sali_menu(today)
 print('\nHima & Sali:\n\n%s' % hima_sali_menu.replace('<br />', '').replace('&amp;', '&').replace('&nbsp;', ''))
+
 dylan_milk_menu = get_dylan_milk_menu(today)
 print('\nDylan Milk:\n\n%s' % dylan_milk_menu.replace('<br />', '\n'))
+
+weather = get_todays_weather()
+print('\nWeather:\n\n temperature %s C\n chance of precipitation %s percent\n wind %s m/s' % (weather['temperature'], weather['precipitation'], weather['wind']))
