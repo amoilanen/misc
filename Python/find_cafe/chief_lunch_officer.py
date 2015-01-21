aea9d23129a59be27da9e66206eab9a72ae69d91
Anton Ivanov <anton.al.ivanov@gmail.com>
2015-01-22 00:03:28 +0200

    Implemented weather rating and unit tests for it

--- Python/find_cafe/chief_lunch_officer.py
+++ Python/find_cafe/chief_lunch_officer.py
@@ -5,6 +5,28 @@
 from constants import TEMPERATURE, PRECIPITATION_CHANCE, PRECIPITATION_AMOUNT, WIND
 from constants import NEPALESE, CHINESE, HIMA_SALI, DYLAN_MILK
 
+class WeatherOpinion:
+
+    MAX_ACCEPTABLE_WIND = 8
+    MAX_ACCEPTABLE_TEMPERATURE = 25
+    MIN_ACCEPTABLE_TEMPERATURE = -15
+    MAX_ACCEPTABLE_PRECIPITATION_CHANCE = 50
+    MAX_ACCEPTABLE_PRECIPITATION_AMOUNT = 2.0
+
+    def __init__(self):
+        self._weather = None
+
+    def weather(self, weather):
+        self._weather = weather
+        return self
+
+    def is_positive(self):
+        return (self._weather[WIND] <= WeatherOpinion.MAX_ACCEPTABLE_WIND and
+                self._weather[TEMPERATURE] <= WeatherOpinion.MAX_ACCEPTABLE_TEMPERATURE and
+                self._weather[TEMPERATURE] >= WeatherOpinion.MIN_ACCEPTABLE_TEMPERATURE and
+                self._weather[PRECIPITATION_CHANCE] <= WeatherOpinion.MAX_ACCEPTABLE_PRECIPITATION_CHANCE and
+                self._weather[PRECIPITATION_AMOUNT] <= WeatherOpinion.MAX_ACCEPTABLE_PRECIPITATION_AMOUNT)
+
 class ChiefLunchOfficer(object):
 
     def __init__(self):
--- Python/find_cafe/test/chief_lunch_officer_test.py
+++ Python/find_cafe/test/chief_lunch_officer_test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from chief_lunch_officer import ChiefLunchOfficer
+from constants import TEMPERATURE, PRECIPITATION_CHANCE, PRECIPITATION_AMOUNT, WIND
+from chief_lunch_officer import ChiefLunchOfficer, WeatherOpinion
 
 class TestChiefLunchOfficer(unittest.TestCase):
 
@@ -14,6 +15,42 @@ class TestChiefLunchOfficer(unittest.TestCase):
           }
         })
         self.assertEqual('cafe1', self.clo.decide())
+
+class TestWeatherOpinion(unittest.TestCase):
+
+    def setUp(self):
+        self.opinion = WeatherOpinion()
+        self.weather = {
+            TEMPERATURE: 10,
+            PRECIPITATION_CHANCE: 10,
+            PRECIPITATION_AMOUNT: 0.2,
+            WIND: 5
+        }
+        self.opinion.weather(self.weather)
+
+    def test_if_too_strong_wind_weather_is_bad(self):
+        self.weather[WIND] = 15
+        self.assertFalse(self.opinion.is_positive())
+
+    def test_if_too_cold_weather_is_bad(self):
+        self.weather[TEMPERATURE] = -30
+        self.assertFalse(self.opinion.is_positive())
+
+    def test_if_too_hot_weather_is_bad(self):
+        self.weather[TEMPERATURE] = 30
+        self.assertFalse(self.opinion.is_positive())
+
+    def test_if_a_lot_of_precipitation_with_high_chance_weather_is_bad(self):
+        self.weather[PRECIPITATION_CHANCE] = 80
+        self.weather[PRECIPITATION_AMOUNT] = 2.0
+        self.assertFalse(self.opinion.is_positive())
+
+    def test_if_temperature_is_mild_minor_chance_of_minor_precipitation_and_wind_mild(self):
+        self.weather[WIND] = 5
+        self.weather[TEMPERATURE] = -2
+        self.weather[PRECIPITATION_CHANCE] = 10
+        self.weather[PRECIPITATION_AMOUNT] = 0.5
+        self.assertTrue(self.opinion.is_positive())
 
 #TODO: If one cafe one day then likely another one next day
 #TODO: If meatballs, likely go there
