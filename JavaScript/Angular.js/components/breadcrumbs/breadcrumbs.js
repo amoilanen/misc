30c6ddc237dfb9b30d029a02b84bbbf67dbd118c
Anton Ivanov <anton.al.ivanov@gmail.com>
2015-02-05 03:53:14 +0200

    Breadcrumbs component for Angular.js

--- JavaScript/Angular.js/components/breadcrumbs/breadcrumbs.js
+++ JavaScript/Angular.js/components/breadcrumbs/breadcrumbs.js
@@ -1,14 +1,25 @@
 var components = angular.module('Components', []);
 
-components.directive('compBreadcrumbs', function() {
+components.controller('breadcrumbsController', function ($scope) {
+  $scope.pathSelected = function (idx) {
+    var newPath = $scope.path.slice(0, idx + 1);
+      if (newPath.join('/') != $scope.path.join('/')) {
+        $scope.onChange({
+          path: newPath
+        });
+      }
+  };
+}).directive('compBreadcrumbs', function () {
   return {
     restrict: 'E',
-    //link: function(scope, element, attrs, controllers) {
-      //TODO: Implement
-    //  },
     scope: {
-      path: '='
+      path: '=',
+      onChange: '&onChange'
     },
-    template: '{{path}}'
+    controller: 'breadcrumbsController',
+    template: '<div class="breadcrumbs">\n' +
+'<span class="crumb" ng-click="pathSelected($index)" ng-repeat-start="pathPart in path">{{pathPart}}</span>' +
+'<span class="crumb-separator" ng-if="$index < path.length - 1" ng-repeat-end>&gt;</span>' +
+'</div>'
   };
 });
