a425f40ee0127ca7267aa0a6918a37136e8d6057
Anton Ivanov <anton.al.ivanov@gmail.com>
2015-02-09 20:04:19 +0200

    Rendered the breadcrumbs statically

--- JavaScript/React.js/components/breadcrumbs/breadcrumbs.js
+++ JavaScript/React.js/components/breadcrumbs/breadcrumbs.js
@@ -1,8 +1,35 @@
-//TODO: Port the Breadcrumbs component to React.js
-var Breadcrumbs = React.createClass({
+var Crumb = React.createClass({
   render: function() {
     return (
-      <div>{this.props.path.join('>')}</div>
+      <span className="crumb">{this.props.value}</span>
+    )
+  }
+});
+
+var CrumbSeparator = React.createClass({
+  render: function() {
+    return (
+      <span className="crumb-separator">&gt;</span>
+    )
+  }
+});
+
+var Breadcrumbs = React.createClass({
+  render: function() {
+
+    var crumbs = this.props.path.map(function(pathPart, index) {
+      return (
+        <span>
+          <Crumb idx={index} value={pathPart} key={index}/>
+          <CrumbSeparator/>
+        </span>
+      );
+    });
+
+    return (
+      <div className="breadcrumbs">
+        {crumbs}
+      </div>
     );
   }
 });
