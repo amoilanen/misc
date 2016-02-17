b00e4005671ee52650fa8d8c743568a251310ab3
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-02-17 23:08:41 +0100

    Updated the script that archives the github repos.

--- Python/archive_github_account.py
+++ Python/archive_github_account.py
@@ -1,65 +1,51 @@
 #!/usr/local/bin/python
 
-# Utility for archiving Github.com repositories for a given account.
+# Linux utility for archiving Github.com repositories for a given account.
 #
 # Example usage:
 #
-#  python archive_github_account.py antivanov ~/github.archive
+#  python archive_github_account.py antivanov
 #
 
-import re
 import sys
 import os
+import json
 import urllib.request
+from string import Template
 
-PUBLIC_REPO_HTML_REGEX = "<li class=\"public source\">.*?<a href=\"(.*?)\">"
+repos_folder = "github_repos"
+repos_url_template = Template("https://api.github.com/users/${github_user}/repos")
+git_clone_template = Template("mkdir -p '${repos_folder}' && cd '${repos_folder}' && git clone ${repo_url} && cd..")
+tar_template = Template("tar -zcvf '${archive_file_name}' '${repos_folder}'")
 
 def get_public_repos(github_user):
-  repositories_url = "https://github.com/" + github_user + "?tab=repositories"
-  repositories_html = urllib.request.urlopen(repositories_url).read().decode("utf-8")
+  url = repos_url_template.substitute(github_user=github_user)
+  repos_json = urllib.request.urlopen(url).read().decode('utf-8')
+  repos = json.loads(repos_json)
+  repo_urls = [repo["html_url"] for repo in repos]
+  return repo_urls
 
-  return re.findall(PUBLIC_REPO_HTML_REGEX, repositories_html, re.M|re.DOTALL)
-
-def get_user_and_archive_folder():
-  github_user, archive_folder_path = read_command_line_args()
-  ensure_exists(archive_folder_path)
-  return (github_user, archive_folder_path)
-
-def ensure_exists(folder_path):
-  if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-
-def read_command_line_args():
+def get_user():
   github_user = sys.argv[1]
+  return github_user
 
-  archive_folder = sys.argv[2] if len(sys.argv) >= 3 else "."
-  archive_folder_path = os.path.abspath(archive_folder)
+def clone_githup_repo(repo_url):
+  print("Cloning '%s' ..." % (repo_url))
+  git_clone_cmd = git_clone_template.substitute(repo_url=repo_url, repos_folder=repos_folder)
+  os.system(git_clone_cmd)
 
-  return (github_user, archive_folder_path)
-
-def clone_githup_repo(repo_path, folder_path):
-  os.system("git clone https://github.com/" + repo_path + " " + folder_path + repo_path)
-
-def zip_archive_folder(github_user, archive_folder_path):
+def zip_archive_folder(github_user):
   archive_file_name = "github.archive." + github_user + ".tar.gz"
-  os.system("tar -zcvf " + archive_file_name + " " + archive_folder_path)
-  os.rename(archive_file_name, archive_folder_path + "/" + archive_file_name)
+  os.system(tar_template.substitute(archive_file_name=archive_file_name, repos_folder=repos_folder))
   return archive_file_name
 
 # Start of the script
-
 print("Archiving Github.com repositories...")
-
-github_user, archive_folder_path = get_user_and_archive_folder()
-
+github_user = get_user()
 print("Github.com user = ", github_user)
-print("Archive folder = ", archive_folder_path)
-
-public_repo_relative_pathes = get_public_repos(github_user)
-
-for repo_path in public_repo_relative_pathes:
-  clone_githup_repo(repo_path, archive_folder_path)
-
-archive_file_name = zip_archive_folder(github_user, archive_folder_path)
-
-print("Done. Repositories are in " + archive_folder_path + "/" + archive_file_name)
+public_repo_urls = get_public_repos(github_user)
+print("Known repos = ", public_repo_urls)
+for repo_url in public_repo_urls:
+  clone_githup_repo(repo_url)
+archive_file_name = zip_archive_folder(github_user)
+print("Done. Repositories are in " + archive_file_name)
