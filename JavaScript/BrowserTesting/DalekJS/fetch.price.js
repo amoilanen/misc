4e3e3639a5f421a194ca7cb619a7e0348806d717
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-06-25 17:37:36 +0300

    DalekJS script is completed.

--- JavaScript/BrowserTesting/DalekJS/fetch.price.js
+++ JavaScript/BrowserTesting/DalekJS/fetch.price.js
@@ -1,6 +1,6 @@
 /*
  * Running the script:
- *   dalek fetch.price.js JavaScript
+ *   dalek fetch.price.js Javascript -l 4
  */
 
 var bookTitle = process.argv[3];
@@ -9,11 +9,55 @@ console.log('Searching Amazon.com for "' + bookTitle + '"');
 module.exports = {
   'Fetch price from Amazon': function(test) {
     test
-      .open('http://amazon.com')
+      .open('http://www.amazon.com')
       .type('#twotabsearchtextbox', bookTitle)
       .click('input[value=Go]')
-      .waitForElement('.s-result-item', 'There are results')
-      .screenshot('search.results.png')
+      .waitFor(function() {
+        function isSearchFinished() {
+          var noResultsTitle = document.querySelector('#noResultsTitle');
+          var results = document.querySelectorAll('.s-result-item');
+
+          return !!noResultsTitle || (results.length > 0);
+        }
+
+        return isSearchFinished();
+      })
+      .execute(function() {
+        function getBookInfo() {
+          var noResultsTitle = document.querySelector('#noResultsTitle');
+
+          if (noResultsTitle) {
+            return {};
+          }
+
+          var bookElement = document.querySelector('.s-result-item');
+
+          var title = bookElement.querySelector('.s-access-title').innerHTML;
+          var price = bookElement.querySelector('.a-color-price').innerHTML;
+          var rating = bookElement.querySelector('.a-icon-star .a-icon-alt').innerHTML;
+
+          return {
+            title: title,
+            price: price,
+            rating: rating
+          };
+        }
+
+        this.data('bookInfo', getBookInfo());
+      })
+      .log.message(function() {
+        var bookInfo = test.data('bookInfo');
+
+        if (!bookInfo) {
+          return 'No results found for "' + bookTitle + '\"';
+        } else {
+          return [
+            'Title = ' + bookInfo.title,
+            'Price = ' + bookInfo.price,
+            'Rating = ' + bookInfo.rating
+          ].join('\n');
+        }
+      })
       .done();
-  }
+    }
 };
