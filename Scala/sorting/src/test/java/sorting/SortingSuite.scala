46c6d58e1a07bce8e8dcde55a9b3e1a3fe3d82a2
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-11-05 21:12:22 +0200

    Stub for a Scala Maven project.

---
+++ Scala/sorting/src/test/java/sorting/SortingSuite.scala
@@ -0,0 +1,14 @@
+package sorting
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class SortingSuite extends FunSuite {
+
+  test("first test") {
+    assert(1 == 1)
+  }
+}
+
