5cce845eab3ca745f88828bd8f90df1f8cbf2090
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-11-06 19:57:32 +0200

    Bubble sort ported from Haskell to Scala.

--- Scala/sorting/src/main/java/sorting/Sorting.scala
+++ Scala/sorting/src/main/java/sorting/Sorting.scala
@@ -1,5 +1,27 @@
 package sorting
 
 object Sorting {
-  
+
+  def bubbleSort[A <% Ordered[A]](seq: Seq[A]): Seq[A] = {
+
+    def bubbleSortIteration[A <% Ordered[A]](seq: Seq[A]): Seq[A] = seq match {
+      case (x::y::rest) =>
+        if (x > y) {
+          y +: bubbleSortIteration(x :: rest)
+        } else {
+          x +: bubbleSortIteration(y :: rest)
+        }
+      case all => all
+    }
+
+    def bubbleSortWithIndex[A <% Ordered[A]](seq: Seq[A], idx: Integer): Seq[A] = {
+      if (idx == seq.length) {
+        seq
+      } else {
+        bubbleSortWithIndex(bubbleSortIteration(seq), idx + 1)
+      }
+    }
+
+    bubbleSortWithIndex(seq, 0)
+  }
 }
--- Scala/sorting/src/test/java/sorting/SortingSuite.scala
+++ Scala/sorting/src/test/java/sorting/SortingSuite.scala
@@ -3,12 +3,13 @@ package sorting
 import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+import sorting.Sorting._
 
 @RunWith(classOf[JUnitRunner])
 class SortingSuite extends FunSuite {
 
   test("first test") {
-    assert(1 == 1)
+    assert(bubbleSort(List(3, 2, 4, 1, 5)) === List(1, 2, 3, 4, 5))
   }
 }
 
