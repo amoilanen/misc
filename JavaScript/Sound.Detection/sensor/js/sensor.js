b0709c4382bd18c79c48e8408038106a587770b8
Anton Ivanov <anton.al.ivanov@gmail.com>
2016-12-07 22:53:16 +0200

    Detect average volume over a period of time and report to the client code.

--- JavaScript/Sound.Detection/sensor/js/sensor.js
+++ JavaScript/Sound.Detection/sensor/js/sensor.js
@@ -1,19 +1,54 @@
 (function(host) {
 
-  function Sensor() {
+  function Sensor(listeners) {
+    this.listeners = listeners;
+    if (!this.listeners.onVolume) {
+      this.listeners.onVolume = function() {};
+    }
+    if (!this.listeners.onMaxAverageVolume) {
+      this.listeners.onMaxAverageVolume = function() {};
+    }
+    this.lastMeasurementTime = null;
+    this.measurementIntervalMs = 5000;
+    this.soundDetectionThreshold = 0.2; //TODO: Should be configurable
+    this.measurements = [];
   }
 
-  Sensor.prototype.listen = function(onCurrentVolumeLevelCallback) {
+  function getVolume(self, analyzer) {
+    var frequencyValues = new Uint8Array(analyzer.frequencyBinCount);
+
+    analyzer.getByteFrequencyData(frequencyValues);
+    return Array.from(frequencyValues).reduce(function(acc, value) {
+      return acc + value / frequencyValues.length;
+    }, 0) / frequencyValues.length;
+  }
+
+  function recordValue(self, volumeValue) {
+    self.measurements.push(volumeValue);
+    var currentMeasurementTime = new Date().getTime();
+    if (currentMeasurementTime - self.lastMeasurementTime >= self.measurementIntervalMs) {
+      var maxVolume = Math.max.apply(null, self.measurements);
+      self.listeners.onMaxAverageVolume({
+        volume: maxVolume,
+        threshold: self.soundDetectionThreshold
+      });
+      self.measurements = [];
+      self.lastMeasurementTime = currentMeasurementTime;
+    }
+  }
+
+  Sensor.prototype.listen = function() {
+    var self = this;
+
+    this.lastMeasurementTime = new Date().getTime();
     Audio.processMicrophoneInput(function(analyzer) {
-      var frequencyValues = new Uint8Array(analyzer.frequencyBinCount);
-      analyzer.getByteFrequencyData(frequencyValues);
+      var volumeValue = getVolume(self, analyzer);
 
-      var averageFrequencyValue = Array.from(frequencyValues).reduce(function(acc, value) {
-        return acc + value / frequencyValues.length;
-      }, 0) / frequencyValues.length;
-
-      var volumeValue = averageFrequencyValue;
-      onCurrentVolumeLevelCallback(volumeValue);
+      recordValue(self, volumeValue);
+      self.listeners.onVolume({
+        volume: volumeValue,
+        threshold: self.soundDetectionThreshold
+      });
     }, function error(e) {
       var errorMessageNode = document.createTextNode(
         'Error while trying to listen to the microphone...' + JSON.stringify(e)
