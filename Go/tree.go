e82440dec5120b547e340b696a70b877be8e994c
Anton Ivanov <anton.al.ivanov@gmail.com>
2017-11-21 00:33:42 +0200

    Recursively read the directory structure.

--- Go/tree.go
+++ Go/tree.go
@@ -1,43 +1,51 @@
 package main
 
+//TODO: Run tree on a file, not a directory
+//TODO: Run tree on directory
+//TODO: Command line arguments, help, like in the tree command
+
 import (
     "fmt"
     "io/ioutil"
     "log"
     "os"
+    "path"
+    "strings"
 )
 
 type File struct {
   name string
-}
-
-type Directory struct {
-  name string
+  path string
   files []File
 }
 
-func (dir Directory) list() {
-  fmt.Println(dir.name)
-  for _, file := range dir.files {
-    fmt.Println(file.name)
+func (file File) Show(offset int) {
+  fmt.Println(strings.Repeat(" ", offset) + file.name)
+  for _, file := range file.files {
+    file.Show(offset + 1)
   }
 }
 
-func main() {
-    dirName := "../"
-    dirFiles, err := ioutil.ReadDir(dirName)
+func ReadDirectory(dirPath string) File {
+    childFiles, err := ioutil.ReadDir(dirPath)
     if err != nil {
         log.Fatal(err)
     }
 
     files := make([]File, 1)
-    for _, f := range dirFiles {
-      if stat, err := os.Stat(f.Name()); err == nil && stat.IsDir() {
-        //files = append(files, Directory{ name: f.Name(), files = make([]File) })
-      } else {
-        files = append(files, File{ name: f.Name() })
-      }
+    for _, f := range childFiles {
+        filePath := dirPath + "/" + f.Name()
+        if stat, err := os.Stat(filePath); err == nil && stat.IsDir() {
+            files = append(files, ReadDirectory(filePath))
+        } else {
+            files = append(files, File{ name: f.Name(), path: filePath })
+        }
     }
-    dir := Directory { name: dirName, files: files }
-    dir.list()
+    return File { name: path.Base(dirPath), path: dirPath, files: files }
+}
+
+func main() {
+    dirName := "../Python"
+    dir := ReadDirectory(dirName)
+    dir.Show(0)
 }
