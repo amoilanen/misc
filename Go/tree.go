ee6240ac0366f9234f0623ee354562f99d3397e5
Anton Ivanov <anton.al.ivanov@gmail.com>
2017-11-28 23:45:31 +0200

    Directory tree structure is printed.

--- Go/tree.go
+++ Go/tree.go
@@ -1,5 +1,6 @@
 package main
 
+//TODO: Print overall statistics at the end of the listing
 //TODO: Run tree on a file, not a directory
 //TODO: Run tree on directory
 //TODO: Command line arguments, help, like in the tree command
@@ -10,19 +11,48 @@ import (
     "log"
     "os"
     "path"
-    "strings"
+    "sort"
 )
 
+type Files []File
 type File struct {
   name string
   path string
-  files []File
+  isDir bool
+  files Files
 }
 
-func (file File) Show(offset int) {
-  fmt.Println(strings.Repeat(" ", offset) + file.name)
-  for _, file := range file.files {
-    file.Show(offset + 1)
+func (files Files) Len() int {
+    return len(files)
+}
+
+func (files Files) Swap(i, j int) {
+    files[i], files[j] = files[j], files[i]
+}
+func (files Files) Less(i, j int) bool {
+    return files[i].name < files[j].name
+}
+
+func (file File) PrintTree() {
+    fmt.Println(".")
+    file.PrintLevel("", 0)
+    fmt.Println()
+}
+
+func (file File) PrintLevel(prefix string, treeLevel int) {
+  totalFiles := len(file.files)
+
+  sort.Sort(file.files)
+  for idx, file := range file.files {
+    var filePrefix string
+    if (idx < totalFiles - 1) {
+      fmt.Println(prefix + "├── " + file.name)
+      filePrefix = prefix + "│   "
+    } else {
+      fmt.Println(prefix + "└── " + file.name)
+      filePrefix = prefix + "    "
+    }
+    file.PrintLevel(filePrefix, treeLevel + 1)
   }
 }
 
@@ -32,20 +62,23 @@ func ReadDirectory(dirPath string) File {
         log.Fatal(err)
     }
 
-    files := make([]File, 1)
+    var isDir bool
+    files := make([]File, 0)
     for _, f := range childFiles {
         filePath := dirPath + "/" + f.Name()
-        if stat, err := os.Stat(filePath); err == nil && stat.IsDir() {
+        stat, _ := os.Stat(filePath);
+        isDir = stat.IsDir()
+        if (isDir) {
             files = append(files, ReadDirectory(filePath))
         } else {
             files = append(files, File{ name: f.Name(), path: filePath })
         }
     }
-    return File { name: path.Base(dirPath), path: dirPath, files: files }
+    return File { name: path.Base(dirPath), isDir: isDir, path: dirPath, files: files }
 }
 
 func main() {
-    dirName := "../Python"
-    dir := ReadDirectory(dirName)
-    dir.Show(0)
+    dirPath := "../Python"
+    dir := ReadDirectory(dirPath)
+    dir.PrintTree()
 }
