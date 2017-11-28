ca3be4d549c547a34343c1c9057ba07e4892c557
Anton Ivanov <anton.al.ivanov@gmail.com>
2017-11-29 00:37:02 +0200

    Compute and print overall statistics for a given directory

--- Go/tree.go
+++ Go/tree.go
@@ -1,9 +1,11 @@
 package main
 
 //TODO: Print overall statistics at the end of the listing
+//TODO: Directory, file single form
+
 //TODO: Run tree on a file, not a directory
 //TODO: Run tree on directory
-//TODO: Command line arguments, help, like in the tree command
+//TODO: Command line arguments, help, like in the tree command, add #!/usr/bin/go
 
 import (
     "fmt"
@@ -16,10 +18,15 @@ import (
 
 type Files []File
 type File struct {
-  name string
-  path string
-  isDir bool
-  files Files
+    name string
+    path string
+    isDir bool
+    files Files
+}
+
+type FileStatistics struct {
+    filesNumber int
+    dirsNumber int
 }
 
 func (files Files) Len() int {
@@ -37,6 +44,39 @@ func (file File) PrintTree() {
     fmt.Println(".")
     file.PrintLevel("", 0)
     fmt.Println()
+}
+
+func (file File) ComputeStatistics() FileStatistics {
+    if (file.isDir) {
+        totalStats := FileStatistics { filesNumber: 0, dirsNumber: 1 }
+        for _, childFile := range file.files {
+            fileStats :=  childFile.ComputeStatistics()
+            totalStats = FileStatistics {
+                filesNumber: fileStats.filesNumber + totalStats.filesNumber,
+                dirsNumber:  fileStats.dirsNumber + totalStats.dirsNumber }
+        }
+        return totalStats
+    } else {
+        return FileStatistics{ filesNumber: 1, dirsNumber: 0 }
+    }
+}
+
+func (stats FileStatistics) PrintStatistics() {
+    dirNumberWithoutSelf := stats.dirsNumber - 1
+    var directoriesSuffix string
+    var filesSuffix string
+    if (dirNumberWithoutSelf == 1) {
+        directoriesSuffix = "y"
+    } else {
+        directoriesSuffix = "ies"
+    }
+    if (stats.filesNumber == 1) {
+        filesSuffix = ""
+    } else {
+        filesSuffix = "s"
+    }
+    formattedStats := fmt.Sprintf("%d director%s, %d file%s", dirNumberWithoutSelf, directoriesSuffix, stats.filesNumber, filesSuffix)
+    fmt.Println(formattedStats)
 }
 
 func (file File) PrintLevel(prefix string, treeLevel int) {
@@ -62,18 +102,19 @@ func ReadDirectory(dirPath string) File {
         log.Fatal(err)
     }
 
-    var isDir bool
     files := make([]File, 0)
     for _, f := range childFiles {
         filePath := dirPath + "/" + f.Name()
-        stat, _ := os.Stat(filePath);
-        isDir = stat.IsDir()
+        stat, _ := os.Stat(filePath)
+        isDir := stat.IsDir()
         if (isDir) {
             files = append(files, ReadDirectory(filePath))
         } else {
             files = append(files, File{ name: f.Name(), path: filePath })
         }
     }
+    stat, _ := os.Stat(dirPath)
+    isDir := stat.IsDir()
     return File { name: path.Base(dirPath), isDir: isDir, path: dirPath, files: files }
 }
 
@@ -81,4 +122,6 @@ func main() {
     dirPath := "../Python"
     dir := ReadDirectory(dirPath)
     dir.PrintTree()
+    stats := dir.ComputeStatistics()
+    stats.PrintStatistics()
 }
