8dc90a0c31744bc25ca8770afdb83e6fb1cc23c2
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-05-01 01:33:27 +0300

    Handle the case when Content-Length header is not available

--- Python/fetch_hyperlinks.py
+++ Python/fetch_hyperlinks.py
@@ -1,6 +1,6 @@
 # Simple RSS feed fetcher.
 # Usage:
-#    python3 fetch_hyperlinks.py --url http://archive.org/download/MIT18.06S05_MP4/ --save_to_dir=/home/anton/Educational/MIT/18-06-linear-algebra-spring-2010 --ext "ogv,srt,gif"
+#    python3 fetch_hyperlinks.py --url http://archive.org/download/MIT18.06S05_MP4/ --save_to_dir=/home/anton/Educational/MIT/18-06-linear-algebra-spring-2010 --ext "mp4,srt,gif"
 #
 import re
 import os
@@ -10,23 +10,31 @@ import math
 from datetime import datetime
 from urllib.request import urlopen
 from urllib.parse import urljoin
+from pathlib import Path
 
 READ_BUFFER_SIZE_BYTES = 16384
 PROGRESS_BAR_LENGTH = 100
 BYTES_IN_MEGABIT = 1024 * 1024 / 8
 
 def show_progress_bar(completed, total, extra_progress_info, bar_length = PROGRESS_BAR_LENGTH):
-    bar_length_unit_value = (total / bar_length)
-    completed_bar_part = math.ceil(completed / bar_length_unit_value)
-    progress = "*" * completed_bar_part
-    remaining = " " * (bar_length - completed_bar_part)
-    percent_done = "%.2f" % ((completed / total) * 100)
-    print(f'[{progress}{remaining}] {percent_done}% {extra_progress_info}', end='\r')
+    if total > 0:
+        bar_length_unit_value = (total / bar_length)
+        completed_bar_part = math.ceil(completed / bar_length_unit_value)
+        progress = "*" * completed_bar_part
+        remaining = " " * (bar_length - completed_bar_part)
+        percent_done = "%.2f" % ((completed / total) * 100)
+        print(f'[{progress}{remaining}] {percent_done}% {extra_progress_info}', end='\r')
+    else:
+        print(extra_progress_info, end='\r')
 
 def download_url(url, destination_file):
+    #TODO: Skip the file only if its size is the same as specified in Content-Length?
+    if (Path(destination_file).is_file()):
+        print(f'{destination_file} was already downloaded, skipping...')
+        return
     open_url = urlopen(url)
     url_info = open_url.info()
-    content_length = int(url_info.get('Content-Length'))
+    content_length = int(url_info.get('Content-Length', -1))
     print(f'Downloading {url} as {destination_file}, {content_length} bytes...')
     start_time = datetime.now()
     with open(destination_file, 'bw') as target:
