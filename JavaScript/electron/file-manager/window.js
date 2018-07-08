c586273edec77a77d6d318561649d4bc784c3414
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-07-08 17:31:22 +0300

    Use async/await in modern Node.js, use promisify

--- JavaScript/electron/file-manager/window.js
+++ JavaScript/electron/file-manager/window.js
@@ -1,12 +1,19 @@
+const { promisify } = require('util');
 const os = require('os');
 const fs = require('fs');
 const electron = require('electron');
+const readdir = promisify(fs.readdir);
 const currentWindow = electron.remote.getCurrentWindow();
 
 console.log('Rendered file manager');
 console.log(currentWindow.directory);
-fs.readdir(currentWindow.directory, (err, files) => {
+
+const listFiles = async dir => {
+  const files = await readdir(dir);
+
   files.forEach(file => {
     console.log(file);
   });
-});
+};
+
+listFiles(currentWindow.directory);
