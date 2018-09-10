49ce9d45ecd9d880676a76644898105b7e69aec9
anton.al.ivanov@gmail.com <anton.al.ivanov@gmail.com>
2018-09-11 00:36:37 +0300

    Excercise 5_4, monad transformers

--- Scala/scala-with-cats/excercises/src/main/scala/learning/excercise_5_4/Autobots.scala
+++ Scala/scala-with-cats/excercises/src/main/scala/learning/excercise_5_4/Autobots.scala
@@ -6,15 +6,19 @@ import scala.concurrent.{ExecutionContext, Future}
 import cats.syntax.either._
 import cats.syntax.applicative._
 import cats.instances.future._
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 case class Autobots(robots: Map[String, Int])(implicit ec: ExecutionContext) {
+
+  val MinimalSpecialPower = 15
 
   type Response[A] = EitherT[Future, String, A]
 
   def getPowerLevel(autobot: String): Response[Int] = {
     val robotPowerLevel = robots.get(autobot) match {
       case Some(value) => value.asRight[String]
-      case None => s"Unknown robot $autobot".asLeft[Int]
+      case None => s"Comms error: $autobot unreachable".asLeft[Int]
     }
     EitherT(Future { robotPowerLevel })
   }
@@ -26,6 +30,23 @@ case class Autobots(robots: Map[String, Int])(implicit ec: ExecutionContext) {
         totalPowerValue <- totalPower
         robotPowerValue <- robotPower
       } yield (totalPowerValue + robotPowerValue)
+    )
+  }
+
+  def canSpecialMove(ally1: String, ally2: String): Response[Boolean] =
+    for {
+      power1 <- getPowerLevel(ally1)
+      power2 <- getPowerLevel(ally2)
+    } yield (power1 + power2 > MinimalSpecialPower)
+
+  def tacticalReport(ally1: String, ally2: String): String = {
+    val canMove: Either[String, Boolean] = Await.result(canSpecialMove(ally1, ally2).value, 1.second)
+
+    canMove.fold(
+      error => error,
+      canMove =>
+        if (canMove) s"$ally1 and $ally2 are ready to roll out!"
+        else s"$ally1 and $ally2 need a recharge."
     )
   }
 }
--- Scala/scala-with-cats/excercises/src/test/scala/learning/excercise_5_4/AutobotsSpec.scala
+++ Scala/scala-with-cats/excercises/src/test/scala/learning/excercise_5_4/AutobotsSpec.scala
@@ -7,15 +7,15 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class AutobotsSpec extends WordSpec with Matchers {
 
+  val powerLevels = Map(
+    "Jazz" -> 6,
+    "Bumblebee" -> 8,
+    "Hot Rod" -> 10
+  )
+
+  val autobots = Autobots(powerLevels)
+
   "getPowerLevel" should {
-
-    val powerLevels = Map(
-      "Jazz" -> 6,
-      "Bumblebee" -> 8,
-      "Hot Rod" -> 10
-    )
-
-    val autobots = Autobots(powerLevels)
 
     "should work with one known robot" in {
       val result = Await.result(autobots.getPowerLevel("Jazz").value, 1.second)
@@ -24,9 +24,9 @@ class AutobotsSpec extends WordSpec with Matchers {
     }
 
     "should work with one unknown robot" in {
-      val result = Await.result(autobots.getPowerLevel("Jazz 2").value, 1.second)
+      val result = Await.result(autobots.getPowerLevel("Ironhide").value, 1.second)
 
-      result shouldEqual Left("Unknown robot Jazz 2")
+      result shouldEqual Left("Comms error: Ironhide unreachable")
     }
 
     "should work with several known robots" in {
@@ -36,9 +36,51 @@ class AutobotsSpec extends WordSpec with Matchers {
     }
 
     "should work with several robots one of which is not known" in {
-      val result = Await.result(autobots.getPowerLevel(Set("Bumblebee", "Hot Rod", "Jazz 2")).value, 1.second)
+      val result = Await.result(autobots.getPowerLevel(Set("Bumblebee", "Hot Rod", "Ironhide")).value, 1.second)
 
-      result shouldEqual Left("Unknown robot Jazz 2")
+      result shouldEqual Left("Comms error: Ironhide unreachable")
+    }
+  }
+
+  "canSpecialMove" should {
+
+    "return true for autobots that can perform a special move" in {
+      val result = Await.result(autobots.canSpecialMove("Hot Rod", "Bumblebee").value, 1.second)
+
+      result shouldEqual Right(true)
+    }
+
+    "return false for autobots that cannot perform a special move" in {
+      val result = Await.result(autobots.canSpecialMove("Jazz", "Bumblebee").value, 1.second)
+
+      result shouldEqual Right(false)
+    }
+
+    "return error if one autobot cannot be reached" in {
+      val result = Await.result(autobots.canSpecialMove("Jazz", "Ironhide").value, 1.second)
+
+      result shouldEqual Left("Comms error: Ironhide unreachable")
+    }
+  }
+
+  "tacticalReport" should {
+
+    "return report for autobots that can perform a special move" in {
+      val result = autobots.tacticalReport("Bumblebee", "Hot Rod")
+
+      result shouldEqual "Bumblebee and Hot Rod are ready to roll out!"
+    }
+
+    "return report for autobots that cannot perform a special move" in {
+      val result = autobots.tacticalReport("Jazz", "Bumblebee")
+
+      result shouldEqual "Jazz and Bumblebee need a recharge."
+    }
+
+    "return report if one autobot cannot be reached" in {
+      val result = autobots.tacticalReport("Jazz", "Ironhide")
+
+      result shouldEqual "Comms error: Ironhide unreachable"
     }
   }
 }
