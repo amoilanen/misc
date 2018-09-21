e2e5c8d7d9bd1ebc26f5bef33d020cc8f3f9cd16
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-09-21 17:16:15 +0300

    Re-factor solution for 6.3.1.1 to use a for comprehension

--- Scala/scala-with-cats/excercises/src/main/scala/learning/excercise_6_3_1_1/ProductViaFlatMap.scala
+++ Scala/scala-with-cats/excercises/src/main/scala/learning/excercise_6_3_1_1/ProductViaFlatMap.scala
@@ -1,14 +1,15 @@
 package learning.excercise_6_3_1_1
 
 import cats.Monad
+import cats.syntax.functor._
+import cats.syntax.flatMap._
 
 object ProductViaFlatMap {
 
   def product[M[_]: Monad, A, B](ma: M[A], mb: M[B]): M[(A, B)] =
-    Monad[M].flatMap(ma) {a =>
-      Monad[M].map(mb) {b =>
-        (a, b)
-      }
-    }
+    for {
+      a <- ma
+      b <- mb
+    } yield (a, b)
 
 }
