21bf28ebda760782d9b7a1cc01e1a56f30246865
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-09-23 01:27:46 +0300

    Implement form validation with Either 6.4.4

--- Scala/scala-with-cats/excercises/src/main/scala/learning/excercise_6_4_4/FormValidation.scala
+++ Scala/scala-with-cats/excercises/src/main/scala/learning/excercise_6_4_4/FormValidation.scala
@@ -2,13 +2,24 @@ package learning.excercise_6_4_4
 
 import cats.syntax.either._
 
+import scala.util.Try
+
 object FormValidation {
 
   case class User(name: String, age: Int)
 
-  def readName(params: Map[String, String]): Either[List[String], String] = ???
+  def readName(params: Map[String, String]): Either[List[String], String] =
+    for {
+      name <- getValue(params, "name")
+      _ <- nonBlank(name)
+    } yield name
 
-  def readAge(params: Map[String, String]): Either[List[String], String] = ???
+  def readAge(params: Map[String, String]): Either[List[String], String] =
+    for {
+      age <- getValue(params, "age")
+      parsedAge <- parseInt(age)
+      _ <- nonNegative(parsedAge)
+    } yield age
 
   def getValue(params: Map[String, String], name: String): Either[List[String], String] =
     params.get(name).fold(
@@ -16,4 +27,19 @@ object FormValidation {
     )(
       _.asRight[List[String]]
     )
+
+  def parseInt(value: String): Either[List[String], Int] =
+    Try(value.toInt).toEither.bimap(_ => List("Not a number"), identity)
+
+  def nonBlank(value: String): Either[List[String], String] =
+    if (value.length > 0)
+      value.asRight[List[String]]
+    else
+      List("Is empty").asLeft[String]
+
+  def nonNegative(value: Int): Either[List[String], Int] =
+    if (value > 0)
+      value.asRight[List[String]]
+    else
+      List("Is negative").asLeft[Int]
 }
--- Scala/scala-with-cats/excercises/src/test/scala/learning/excercise_6_4_4/FormValidationSpec.scala
+++ Scala/scala-with-cats/excercises/src/test/scala/learning/excercise_6_4_4/FormValidationSpec.scala
@@ -17,4 +17,37 @@ class FormValidationSpec extends WordSpec with Matchers {
       getValue(params, "e") shouldEqual Left(List("e not found"))
     }
   }
+
+  "parseInt" should {
+
+    "should parse number successfully" in {
+      parseInt("10") shouldEqual Right(10)
+    }
+
+    "should return left if not a number" in {
+      parseInt("e") shouldEqual Left(List("Not a number"))
+    }
+  }
+
+  "nonBlank" should {
+
+    "should be successful for non empty value" in {
+      nonBlank("abc") shouldEqual Right("abc")
+    }
+
+    "should return left for empty value" in {
+      nonBlank("") shouldEqual Left(List("Is empty"))
+    }
+  }
+
+  "nonNegative" should {
+
+    "should be successful for non empty value" in {
+      nonNegative(10) shouldEqual Right(10)
+    }
+
+    "should return left for empty value" in {
+      nonNegative(-5) shouldEqual Left(List("Is negative"))
+    }
+  }
 }
