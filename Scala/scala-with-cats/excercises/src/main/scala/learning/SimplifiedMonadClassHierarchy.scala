25e451dc42ef31b29de4b6a2f5820b8626bb46d9
anton.al.ivanov@gmail.com <anton.al.ivanov@gmail.com>
2018-09-23 19:07:05 +0300

    Added example how Monad implements ap, product and map in terms of flatMap and pure

--- Scala/scala-with-cats/excercises/src/main/scala/learning/SimplifiedMonadClassHierarchy.scala
+++ Scala/scala-with-cats/excercises/src/main/scala/learning/SimplifiedMonadClassHierarchy.scala
@@ -13,15 +13,19 @@ object SimplifiedMonadClassHierarchy {
     def map[A, B](fa: F[A])(f: A => B): F[B]
   }
 
+  /*
+   * Defines product in terms of ap and map
+   */
   trait Apply[F[_]] extends Semigroupal[F] with Functor[F] {
 
     def ap[A, B](ff: F[A => B])(fa: F[A]): F[B]
 
-    def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    override def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
       ap(map(fa)(a => (b: B) => (a, b)))(fb)
   }
 
   trait Applicative[F[_]] extends Apply[F] {
+
     def pure[A](a: A): F[A]
   }
 
@@ -30,6 +34,22 @@ object SimplifiedMonadClassHierarchy {
     def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
   }
 
+  /*
+   * Defines ap, product, map in terms of pure and flatMap
+   */
   trait Monad[F[_]] extends Applicative[F] with FlatMap[F] {
+
+    override def ap[A, B](ff: F[A => B])(fa: F[A]): F[B] =
+      flatMap(ff)(f => map(fa)(f))
+
+    override def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+      flatMap(fa) { a =>
+        map(fb) { b =>
+          (a, b)
+        }
+      }
+
+    override def map[A, B](fa: F[A])(f: A => B): F[B] =
+      flatMap(fa)(f.andThen(pure))
   }
 }
