a691233a65f90178c530ad27f647139acddd883a
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-09-24 13:53:40 +0300

    Implement 'or'

--- Scala/scala-with-cats/excercises/src/main/scala/learning/case_study_data_validation/Validation.scala
+++ Scala/scala-with-cats/excercises/src/main/scala/learning/case_study_data_validation/Validation.scala
@@ -1,15 +1,19 @@
 package learning.case_study_data_validation
 
 import cats.data.Validated
+import cats.data.Validated._
 import cats.Semigroupal
 import cats.Semigroup
-import cats.syntax.semigroupal._
 
 object Validation {
 
   trait Check[E, A] { self =>
 
-    def apply(value: A): Validated[E, A]
+    type ValidatedValue[A] = Validated[E, A]
+
+    def apply(value: A): ValidatedValue[A]
+
+    def map[B](check: Check[E, A])(func: A => B): Check[E, B]= ???
 
     def and(that: Check[E, A])(implicit me: Semigroup[E]): Check[E, A] = new Check[E, A] {
 
@@ -24,7 +28,20 @@ object Validation {
       }
     }
 
-    // other methods...
+    def or(that: Check[E, A])(implicit me: Semigroup[E]): Check[E, A] = new Check[E, A] {
+
+      override def apply(value: A): ValidatedValue[A] = {
+        self(value) match {
+          case Valid(selfValid) => Valid(selfValid)
+          case Invalid(selfError) => {
+            that(value) match {
+              case Valid(thatValid) => Valid(thatValid)
+              case Invalid(thatError) => Invalid(me.combine(selfError, thatError))
+            }
+          }
+        }
+      }
+    }
   }
 
 
