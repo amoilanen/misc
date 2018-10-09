3d00910d5a25e4517ac8385d88caff9e0261e2e2
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-10-09 23:32:12 +0300

    Excercise 7.1.2 foldLeft and foldRight with cons and empty list

---
+++ Scala/scala-with-cats/excercises/src/test/scala/learning/excercise_7_1_2/FoldsSpec.scala
@@ -0,0 +1,24 @@
+package learning.excercise_7_1_2
+
+import org.scalatest._
+
+
+class FoldsSpec extends WordSpec with Matchers {
+
+  "empty accumulator and cons ::" should {
+
+    val list = List(1, 2, 3, 4, 5)
+
+    "foldLeft produce list in reverse order" in {
+      list.foldLeft(List[Int]()) { (acc: List[Int], e: Int) =>
+        e :: acc
+      } shouldEqual list.reverse
+    }
+
+    "foldRight produce list in order" in {
+      list.foldRight(List[Int]()) { (e: Int, acc: List[Int]) =>
+        e :: acc
+      } shouldEqual list
+    }
+  }
+}
