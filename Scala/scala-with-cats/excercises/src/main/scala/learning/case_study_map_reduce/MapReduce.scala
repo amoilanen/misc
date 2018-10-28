c174962f14e7ad778be4de1e2852e8a4a530d8f8
Anton Ivanov <anton.al.ivanov@gmail.com>
2018-10-28 20:13:29 +0200

    Parallel Map/Reduce implementation for single machine

--- Scala/scala-with-cats/excercises/src/main/scala/learning/case_study_map_reduce/MapReduce.scala
+++ Scala/scala-with-cats/excercises/src/main/scala/learning/case_study_map_reduce/MapReduce.scala
@@ -1,14 +1,33 @@
 package learning.case_study_map_reduce
 
+import scala.language.postfixOps
+
 import cats.Monoid
+
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration._
 
 object MapReduce {
 
-  def foldMap[A, B: Monoid](input: Vector[A])(f: A => B): B = {
-    val m = implicitly[Monoid[B]]
-    val mappedInput = input.map(f)
-    mappedInput.reduce((partialResult, mappedItem) =>
+  val TimeoutSeconds = 10
+
+  def reduce[B](input: Vector[B])(implicit m: Monoid[B]): B =
+    input.reduce((partialResult, mappedItem) =>
       m.combine(partialResult, mappedItem)
     )
+
+  def foldMap[A, B: Monoid](input: Vector[A])(f: A => B): B =
+    reduce(input.map(f))
+
+  def parallelFoldMap[A, B: Monoid](input: Vector[A])(f: A => B)(implicit ec: ExecutionContext): B = {
+    val availableCPUs = Runtime.getRuntime.availableProcessors()
+
+    val batches = input.grouped(availableCPUs)
+    val submittedBatches: Iterator[Future[B]] = batches.map(batch => Future {
+      foldMap(batch)(f)
+    })
+    val completedBatches: Iterator[B] = Await.result(Future.sequence(submittedBatches), TimeoutSeconds seconds)
+
+    reduce(completedBatches.toVector)
   }
 }
--- Scala/scala-with-cats/excercises/src/test/scala/learning/case_study_map_reduce/MapReduceSpec.scala
+++ Scala/scala-with-cats/excercises/src/test/scala/learning/case_study_map_reduce/MapReduceSpec.scala
@@ -1,10 +1,13 @@
 package learning.case_study_map_reduce
 
-import org.scalatest.{Matchers, WordSpec}
+import java.util.concurrent.Executors
 
+import org.scalatest.{Matchers, WordSpec}
 import cats.instances.int._
 import cats.instances.string._
 import MapReduce._
+
+import scala.concurrent.ExecutionContext
 
 class MapReduceSpec extends WordSpec with Matchers {
 
@@ -16,6 +19,20 @@ class MapReduceSpec extends WordSpec with Matchers {
 
     "should work with a vector of Strings" in {
       foldMap(Vector(1, 2, 3))(_.toString + "! ") shouldEqual "1! 2! 3! "
+    }
+
+    "should map over string to produce string" in {
+      foldMap("Hello world!".toVector)(_.toString.toUpperCase)
+    }
+  }
+
+  "parallelFoldMap" should {
+
+    val parallelism = Runtime.getRuntime.availableProcessors()
+    implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(parallelism))
+
+    "should work with a vector of Integers" in {
+      parallelFoldMap((1 to 200).toVector)(identity) shouldEqual 20100
     }
   }
 }
