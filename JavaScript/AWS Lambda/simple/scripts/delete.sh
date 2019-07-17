68420302f78f7d4a4745dcf11b2cd19477d97f04
Anton Ivanov <anton.al.ivanov@gmail.com>
2019-07-17 22:32:30 +0300

    Add comments

--- JavaScript/AWS Lambda/simple/scripts/delete.sh
+++ JavaScript/AWS Lambda/simple/scripts/delete.sh
@@ -1,1 +1,2 @@
+# Optionally we can also delete the S3 bucket and the source code, however the bucket might be shared with other Lambda functions
 aws cloudformation delete-stack --stack-name simple-lambda-demo
