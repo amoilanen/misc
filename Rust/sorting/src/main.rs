a42f1c1835bd1c6040c22b95614848c5e802aa55
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-01-05 23:36:02 +0200

    Use generics

--- Rust/sorting/src/main.rs
+++ Rust/sorting/src/main.rs
@@ -1,15 +1,11 @@
-
-//TODO: Use a generic trait to designate a sortable value?
-//TODO: Think about ownership, should we consider doing sorting in place? Is Vec<i32> an appropriate
-//return type
-fn sort(values: &[i32]) -> Vec<i32> {
+fn quick_sort<T: Ord + Copy>(values: &[T]) -> Vec<T> {
     let length = values.len();
     if length == 0 {
         return Vec::new();
     } else {
-        let pivot: i32 = values[0];
-        let mut values_less_than_pivot: Vec<i32> = Vec::new();
-        let mut values_greater_than_pivot: Vec<i32> = Vec::new();
+        let pivot: T = values[0];
+        let mut values_less_than_pivot: Vec<T> = Vec::new();
+        let mut values_greater_than_pivot: Vec<T> = Vec::new();
 
         for &value in values {
             if value < pivot {
@@ -18,15 +14,15 @@ fn sort(values: &[i32]) -> Vec<i32> {
                 values_greater_than_pivot.push(value)
             }
         }
-        let mut result = sort(&values_less_than_pivot);
+        let mut result = quick_sort(&values_less_than_pivot);
         result.append(&mut vec![pivot]);
-        result.append(&mut sort(&values_greater_than_pivot));
+        result.append(&mut quick_sort(&values_greater_than_pivot));
         return result;
     }
 }
 
 fn main() {
     let vec = vec![9, 3, 4, 6, 1, 2, 7, 5, 0, 8];
-    let sorted = sort(&vec);
+    let sorted = quick_sort(&vec);
     println!("{:?}", sorted);
 }
