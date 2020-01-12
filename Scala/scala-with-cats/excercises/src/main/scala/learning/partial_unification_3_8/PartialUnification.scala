4346226dad2d961a912a0acb3603aaf520ed93ab
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-01-12 23:04:21 +0200

    Minor adjustment to the partial unification example from the book

--- Scala/scala-with-cats/excercises/src/main/scala/learning/partial_unification_3_8/PartialUnification.scala
+++ Scala/scala-with-cats/excercises/src/main/scala/learning/partial_unification_3_8/PartialUnification.scala
@@ -3,6 +3,7 @@ package learning.partial_unification_3_8
 //import cats.Functor
 import cats.instances.function._
 import cats.syntax.functor._
+import cats.syntax.contravariant._
 
 object PartialUnification extends App {
 
@@ -16,15 +17,17 @@ object PartialUnification extends App {
   val func3b: Int => Double =
     func2.compose(func1)
 
+  println(func3(2))
+
   //Won't compile because of the compiler's left to right bias
   //val func3c = func2.contramap(func1)
 
   type <=[B, A] = A => B
+  val func1b: Double <= Int = func1
   val func2b: Double <= Double = func2
 
   //This should compile
-  //val func3d = func2b.contramap(func1)
+  val func3d = func2b.contramap(func1)
 
-  println(func3(2))
   //println(func3d(3))
 }
