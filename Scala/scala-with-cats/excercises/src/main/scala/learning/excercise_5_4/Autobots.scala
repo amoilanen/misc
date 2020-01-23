67981cf8c545ac833b357c8a91f5735384128f41
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-01-23 23:02:44 +0200

    Improve the solution for Excercise 5.4: reduce duplication, use higher-level functions

--- Scala/scala-with-cats/excercises/src/main/scala/learning/excercise_5_4/Autobots.scala
+++ Scala/scala-with-cats/excercises/src/main/scala/learning/excercise_5_4/Autobots.scala
@@ -15,38 +15,36 @@ case class Autobots(robots: Map[String, Int])(implicit ec: ExecutionContext) {
 
   type Response[A] = EitherT[Future, String, A]
 
-  def getPowerLevel(autobot: String): Response[Int] = {
-    val robotPowerLevel = robots.get(autobot) match {
-      case Some(value) => value.asRight[String]
-      case None => s"Comms error: $autobot unreachable".asLeft[Int]
-    }
-    EitherT(Future { robotPowerLevel })
-  }
+  def getPowerLevel(autobot: String): Response[Int] =
+    EitherT(Future {
+      robots.get(autobot).fold(
+        s"Comms error: $autobot unreachable".asLeft[Int]
+      )(_.asRight)
+    })
 
-  def getPowerLevel(autobots: Set[String]): Response[Int] = {
-    val powerLevels = autobots.map(getPowerLevel(_))
-    powerLevels.foldLeft(0.pure[Response])((robotPower, totalPower) =>
-      for {
-        totalPowerValue <- totalPower
-        robotPowerValue <- robotPower
-      } yield (totalPowerValue + robotPowerValue)
-    )
-  }
+  def getPowerLevel(autobots: Set[String]): Response[Int] =
+      autobots
+        .map(getPowerLevel(_))
+        .foldLeft(0.pure[Response])({ (robotPower, totalPower) =>
+          for {
+            totalPowerValue <- totalPower
+            robotPowerValue <- robotPower
+          } yield (totalPowerValue + robotPowerValue)
+        })
 
-  def canSpecialMove(ally1: String, ally2: String): Response[Boolean] =
-    for {
-      power1 <- getPowerLevel(ally1)
-      power2 <- getPowerLevel(ally2)
-    } yield (power1 + power2 > MinimalSpecialPower)
+  def canSpecialMove(allies: String*): Response[Boolean] =
+    getPowerLevel(allies.toSet).map(_ > MinimalSpecialPower)
 
-  def tacticalReport(ally1: String, ally2: String): String = {
-    val canMove: Either[String, Boolean] = Await.result(canSpecialMove(ally1, ally2).value, 1.second)
+  def tacticalReport(allies: String*): String = {
+    val messageIntro: String = allies.mkString(" and ")
+    val report: Future[String] = canSpecialMove(allies: _*).map(
+      if (_) {
+        s"${messageIntro} are ready to roll out!"
+      } else {
+        s"${messageIntro} need a recharge."
+      }
+    ).fold(identity[String], identity[String])
 
-    canMove.fold(
-      error => error,
-      canMove =>
-        if (canMove) s"$ally1 and $ally2 are ready to roll out!"
-        else s"$ally1 and $ally2 need a recharge."
-    )
+    Await.result(report, 1.second)
   }
 }
