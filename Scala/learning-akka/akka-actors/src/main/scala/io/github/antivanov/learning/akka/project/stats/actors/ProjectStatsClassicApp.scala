81253167e790a054286ea64de3cdcb9cbfe56afe
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-06-16 23:02:27 +0300

    Read all the files in a given directory and count lines in every file

--- Scala/learning-akka/akka-actors/src/main/scala/io/github/antivanov/learning/akka/project/stats/actors/ProjectStatsClassicApp.scala
+++ Scala/learning-akka/akka-actors/src/main/scala/io/github/antivanov/learning/akka/project/stats/actors/ProjectStatsClassicApp.scala
@@ -1,14 +1,59 @@
 package io.github.antivanov.learning.akka.project.stats.actors
 
+import java.io.File
+
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import io.github.antivanov.learning.akka.project.stats.actors.ProjectStatsClassicApp.FileStatsReader.ComputeStatsFor
+
+import scala.io.Source
+
+object FileWalker {
+
+  val DefaultExcludePaths = List(".idea", "target", "project", "node_modules").map(pathPart => f"/$pathPart")
+
+  def listFiles(file: File, excludePaths: List[String] = DefaultExcludePaths): List[File] =
+    if (file.isDirectory) {
+      file.listFiles().map(listFiles(_)).flatten
+        .filter(file =>
+          !excludePaths.exists(file.getAbsolutePath.contains(_))
+        ).toList
+    } else {
+      List(file)
+    }
+}
 
 object ProjectStatsClassicApp extends App {
 
+  class FileStatsReader extends Actor with ActorLogging {
+
+    override def receive: Receive = {
+      case ComputeStatsFor(file) =>
+        val lineCount = Source.fromFile(file).getLines.size
+        val extension = file.getPath.substring(file.getPath.lastIndexOf('.') + 1)
+        println(file.getAbsolutePath())
+        println(extension)
+        println(lineCount)
+    }
+  }
+
+  object FileStatsReader {
+    sealed trait Event
+    case class ComputeStatsFor(file: File) extends Event
+    case class FileStats(extension: String, linesCount: Int) extends Event
+
+    def props: Props = Props(new FileStatsReader)
+  }
+
   class ProjectReader extends Actor with ActorLogging {
+
+    val fileStatsReader = context.actorOf(FileStatsReader.props, "file-stats-reader")
 
     override def receive: Receive = {
       case ProjectReader.ReadProject(projectPath) =>
         log.debug(f"Reading project structure at $projectPath")
+        FileWalker.listFiles(new File(projectPath)).map(file =>
+          fileStatsReader ! FileStatsReader.ComputeStatsFor(file)
+        )
     }
 
   }
@@ -20,12 +65,13 @@ object ProjectStatsClassicApp extends App {
     def props: Props = Props(new ProjectReader)
   }
 
-  //TODO: Read the files and directories in ProjectReader
   //TODO: RouterPool of actors which read file contents
   //TODO: Separate actors for computing stats for different file extensions
-  //TODO: Separate actor to compute the summary of the project stats
+  //TODO: Separate actor to compute the summary of all the project stats:
+  // average code lines per ext, total lines for each ext, total files of each ext, total lines and total files
+  //TODO: Supervision and error handling
 
   val system = ActorSystem()
-  val projectReader = system.actorOf(ProjectReader.props, "main-actor")
+  val projectReader = system.actorOf(ProjectReader.props, "project-reader")
   projectReader ! ProjectReader.ReadProject(".")
 }
