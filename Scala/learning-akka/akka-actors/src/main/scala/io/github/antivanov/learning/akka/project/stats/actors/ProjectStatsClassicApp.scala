f53c5ca8654ec6dbb43c4f8d391bfdc4f1cf82a1
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-06-23 23:58:34 +0300

    Compute line counts by file extension

--- Scala/learning-akka/akka-actors/src/main/scala/io/github/antivanov/learning/akka/project/stats/actors/ProjectStatsClassicApp.scala
+++ Scala/learning-akka/akka-actors/src/main/scala/io/github/antivanov/learning/akka/project/stats/actors/ProjectStatsClassicApp.scala
@@ -2,14 +2,15 @@ package io.github.antivanov.learning.akka.project.stats.actors
 
 import java.io.File
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
-import io.github.antivanov.learning.akka.project.stats.actors.ProjectStatsClassicApp.FileStatsReader.ComputeStatsFor
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.pattern.ask
 
+import scala.concurrent.duration._
 import scala.io.Source
 
 object FileWalker {
 
-  val DefaultExcludePaths = List(".idea", "target", "project", "node_modules").map(pathPart => f"/$pathPart")
+  val DefaultExcludePaths = List(".idea", "target", "project", "node_modules", ".gitignore").map(pathPart => f"/$pathPart")
 
   def listFiles(file: File, excludePaths: List[String] = DefaultExcludePaths): List[File] =
     if (file.isDirectory) {
@@ -24,36 +25,74 @@ object FileWalker {
 
 object ProjectStatsClassicApp extends App {
 
-  class FileStatsReader extends Actor with ActorLogging {
+  case class FileExtension(value: String) extends AnyVal
+
+  class StatsSummaryComputer(ref: ActorRef) extends Actor with ActorLogging {
+
+    import StatsSummaryComputer._
+
+    var totalFileNumber = 0L
+    var handledFileNumber = 0L
+    var lineCounts: Map[FileExtension, Long] = Map().withDefaultValue(0L)
+
+    override def receive: Receive = {
+      case TotalNumberOfFiles(fileNumber) =>
+        totalFileNumber = fileNumber
+      case FileStats(_, extension, linesCount) =>
+        lineCounts += extension -> (lineCounts(extension) + linesCount)
+        handledFileNumber += 1
+        if (handledFileNumber == totalFileNumber) {
+          ref ! ProjectReader.StatsReady(lineCounts)
+        }
+    }
+  }
+
+  object StatsSummaryComputer {
+    sealed trait Event
+    case class TotalNumberOfFiles(fileNumber: Long) extends Event
+    case class FileStats(file: File, extension: FileExtension, linesCount: Long) extends Event
+
+    def props(ref: ActorRef): Props = Props(new StatsSummaryComputer(ref))
+  }
+
+  class FileStatsReader(ref: ActorRef) extends Actor with ActorLogging {
+    import FileStatsReader._
 
     override def receive: Receive = {
       case ComputeStatsFor(file) =>
         val lineCount = Source.fromFile(file).getLines.size
         val extension = file.getPath.substring(file.getPath.lastIndexOf('.') + 1)
-        println(file.getAbsolutePath())
-        println(extension)
-        println(lineCount)
+
+        ref ! StatsSummaryComputer.FileStats(file, FileExtension(extension), lineCount)
     }
   }
 
   object FileStatsReader {
     sealed trait Event
     case class ComputeStatsFor(file: File) extends Event
-    case class FileStats(extension: String, linesCount: Int) extends Event
 
-    def props: Props = Props(new FileStatsReader)
+    def props(ref: ActorRef): Props = Props(new FileStatsReader(ref))
   }
 
   class ProjectReader extends Actor with ActorLogging {
 
-    val fileStatsReader = context.actorOf(FileStatsReader.props, "file-stats-reader")
+    import ProjectReader._
+
+    val statsSummaryComputer = context.actorOf(StatsSummaryComputer.props(self), "summary-computer")
+    val fileStatsReader = context.actorOf(FileStatsReader.props(statsSummaryComputer), "file-stats-reader")
+    var finalLineCounts: Map[FileExtension, Long] = Map()
 
     override def receive: Receive = {
-      case ProjectReader.ReadProject(projectPath) =>
+      case ReadProject(projectPath) =>
         log.debug(f"Reading project structure at $projectPath")
-        FileWalker.listFiles(new File(projectPath)).map(file =>
+        val files = FileWalker.listFiles(new File(projectPath)).map(file =>
           fileStatsReader ! FileStatsReader.ComputeStatsFor(file)
         )
+        statsSummaryComputer ! StatsSummaryComputer.TotalNumberOfFiles(files.size)
+      case StatsReady(lineCounts) =>
+        finalLineCounts = lineCounts
+      case GetStats =>
+        sender() ! finalLineCounts
     }
 
   }
@@ -61,6 +100,8 @@ object ProjectStatsClassicApp extends App {
   object ProjectReader {
     sealed trait Event
     case class ReadProject(projectPath: String)
+    case class StatsReady(lineCounts: Map[FileExtension, Long]) extends Event
+    case object GetStats extends Event
 
     def props: Props = Props(new ProjectReader)
   }
@@ -74,4 +115,10 @@ object ProjectStatsClassicApp extends App {
   val system = ActorSystem()
   val projectReader = system.actorOf(ProjectReader.props, "project-reader")
   projectReader ! ProjectReader.ReadProject(".")
+  Thread.sleep(2000)
+
+  val lineCounts = projectReader.ask(ProjectReader.GetStats)(100.millisecond).mapTo[Map[FileExtension, Long]]
+  println("Final line counts:")
+  println(lineCounts)
+  system.terminate()
 }
