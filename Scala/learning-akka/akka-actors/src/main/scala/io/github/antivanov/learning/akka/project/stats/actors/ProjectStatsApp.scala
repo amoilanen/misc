1d214190a2d70128285e01dc130a0316afeb23df
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-07-12 22:05:17 +0300

    Migrate project stats example to the type actors

--- Scala/learning-akka/akka-actors/src/main/scala/io/github/antivanov/learning/akka/project/stats/actors/ProjectStatsApp.scala
+++ Scala/learning-akka/akka-actors/src/main/scala/io/github/antivanov/learning/akka/project/stats/actors/ProjectStatsApp.scala
@@ -2,10 +2,11 @@ package io.github.antivanov.learning.akka.project.stats.actors
 
 import java.io.File
 
-import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 
-import scala.concurrent.Promise
+import scala.concurrent.{ExecutionContext, Promise}
+import scala.io.Source
 
 object ProjectStatsApp extends App {
 
@@ -14,8 +15,13 @@ object ProjectStatsApp extends App {
     sealed trait Event
     case class ComputeStatsFor(file: File) extends Event
 
-    def apply(): Behavior[ComputeStatsFor] = {
-      Behaviors.ignore
+    def apply(ref: ActorRef[StatsSummaryComputer.FileStats]): Behavior[ComputeStatsFor] = Behaviors.receiveMessage[ComputeStatsFor] { computeStatsFor =>
+      val file = computeStatsFor.file
+      val lineCount = Source.fromFile(file).getLines.size
+      val extension = file.getPath.substring(file.getPath.lastIndexOf('.') + 1)
+
+      ref ! StatsSummaryComputer.FileStats(file, FileExtension(extension), lineCount)
+      Behaviors.same
     }
   }
 
@@ -25,39 +31,70 @@ object ProjectStatsApp extends App {
     case class TotalNumberOfFiles(fileNumber: Long) extends Event
     case class FileStats(file: File, extension: FileExtension, linesCount: Long) extends Event
 
-    def apply(): Behavior[TotalNumberOfFiles] = {
-      Behaviors.ignore
+    def apply(ref: ActorRef[ProjectReader.Event]): Behavior[Event] =
+      receive(ref, totalFileNumber = 0L, handledFileNumber = 0L, lineCounts = Map().withDefaultValue(0L))
+
+    def receive(ref: ActorRef[ProjectReader.Event], totalFileNumber: Long, handledFileNumber: Long, lineCounts: Map[FileExtension, Long]): Behavior[Event] = Behaviors.setup { context =>
+      Behaviors.receiveMessage[Event] {
+        case TotalNumberOfFiles(updatedTotalFileNumber) =>
+          context.log.debug(s"Total number of files $updatedTotalFileNumber")
+          receive(ref, updatedTotalFileNumber, handledFileNumber, lineCounts)
+        case FileStats(file, extension, linesCount) =>
+          val updatedlineCounts = lineCounts + (extension -> (lineCounts(extension) + linesCount))
+          val updatedHandledFileNumber = handledFileNumber + 1
+          if (updatedHandledFileNumber == totalFileNumber) {
+            ref ! ProjectReader.StatsReady(lineCounts)
+          }
+          context.log.debug(s"File stats ${file.getAbsolutePath}, $linesCount")
+          receive(ref, totalFileNumber, updatedHandledFileNumber, updatedlineCounts)
+      }
     }
   }
 
   object ProjectReader {
 
     sealed trait Event
-    case class ReadProject(projectPath: String, lineCountsPromise: Promise[Map[FileExtension, Long]])
+    case class ReadProject(projectPath: String, lineCountsPromise: Promise[Map[FileExtension, Long]]) extends Event
     case class StatsReady(lineCounts: Map[FileExtension, Long]) extends Event
 
-    def apply(): Behavior[ReadProject] = Behaviors.setup { context =>
+    def apply(): Behavior[Event] = readProject
 
-      val fileStatsReader = context.spawn(FileStatsReader(), "file-stats-reader")
-      val statsSummaryComputer = context.spawn(StatsSummaryComputer(), "stats-summary-computer")
+    def readProject: Behavior[Event] = Behaviors.setup { context =>
+      val statsSummaryComputer = context.spawn(StatsSummaryComputer(context.self), "stats-summary-computer")
+      val fileStatsReader = context.spawn(FileStatsReader(statsSummaryComputer), "file-stats-reader")
 
-      Behaviors.receiveMessage[ReadProject] {
+      Behaviors.receiveMessage[Event] {
         case ReadProject(projectPath, lineCountsPromise) =>
           context.log.debug(f"Reading project structure at $projectPath")
-          val promise = Some(lineCountsPromise)
-          val files = FileWalker.listFiles(new File(projectPath)).map(file => {
-            println(file)
+          val files = FileWalker.listFiles(new File(projectPath))
+          statsSummaryComputer ! StatsSummaryComputer.TotalNumberOfFiles(files.size)
+          files.foreach(file => {
             fileStatsReader ! FileStatsReader.ComputeStatsFor(file)
           })
-          statsSummaryComputer ! StatsSummaryComputer.TotalNumberOfFiles(files.size)
-          //TODO: Move to the behavior expecting the response from StatsSummaryComputer: StatsReady
+
+          waitForStatsReady(lineCountsPromise)
+        case StatsReady(_) =>
+          context.log.error("Project reading has not started yet...")
           Behaviors.same
       }
     }
+
+    def waitForStatsReady(promise: Promise[Map[FileExtension, Long]]): Behavior[Event] = Behaviors.receiveMessage[Event] {
+      case StatsReady(lineCounts) =>
+        promise.success(lineCounts)
+        Behaviors.same
+    }
+
   }
 
   val values = Seq(1, 2, 3, 4, 5)
   val lineCountsPromise = Promise[Map[FileExtension, Long]]()
   val actorSystem = ActorSystem(ProjectReader(), "main-actor")
+  implicit val ec: ExecutionContext = actorSystem.executionContext
   actorSystem ! ProjectReader.ReadProject(".", lineCountsPromise)
+  lineCountsPromise.future.andThen { lineCounts =>
+    println("Final line counts:")
+    println(lineCounts)
+    actorSystem.terminate()
+  }
 }
