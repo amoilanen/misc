512eb4edc1558e872986421df25ae99ac291f34c
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-02-23 23:14:14 +0200

    Re-factor solution for printing messages

--- Scala/handsonscala-excercises/src/main/scala/ch3/PrintMessages.scala
+++ Scala/handsonscala-excercises/src/main/scala/ch3/PrintMessages.scala
@@ -2,25 +2,30 @@ package ch3
 
 object PrintMessages {
 
-  val NextLevelIndentation = 4
+  val IndentationPerLevel = 4
 
   class Msg(val id: Int, val parent: Option[Int], val txt: String)
 
-  def renderMessage(msg: Msg, level: Int, messagesGroupedByParent: Map[Option[Int], Array[Msg]]): String = {
-    val childMessages = messagesGroupedByParent.get(Some(msg.id)).getOrElse(Array())
-    val childMessageRenditions = childMessages
-      .map(renderMessage(_, level + 1, messagesGroupedByParent))
-      .mkString("")
-    val prefix = " " * NextLevelIndentation * level
-    s"""${prefix}#${msg.id} ${msg.txt}
-       |${childMessageRenditions}""".stripMargin
+  class Messages(messages: Array[Msg]) {
+    private lazy val messagesGroupedByParent = messages.groupBy(_.parent)
+    private lazy val topLevelMessages = messagesGroupedByParent.get(None).getOrElse(Array())
+
+    private def renderMessage(msg: Msg, level: Int): String = {
+      val childMessages = messagesGroupedByParent.get(Some(msg.id)).getOrElse(Array())
+      val childMessageRenditions = childMessages
+        .map(renderMessage(_, level + 1))
+        .mkString("")
+      val currentMessageIndentation = " " * IndentationPerLevel * level
+      s"""${currentMessageIndentation}#${msg.id} ${msg.txt}
+         |${childMessageRenditions}""".stripMargin
+    }
+
+    def render(): String =
+      topLevelMessages.map(renderMessage(_, 0)).mkString("")
   }
 
-  def renderMessages(messages: Array[Msg]): String = {
-    val messagesGroupedByParent = messages.groupBy(_.parent)
-    val topLevelMessages = messagesGroupedByParent.get(None).getOrElse(Array())
-    topLevelMessages.map(renderMessage(_, 0, messagesGroupedByParent)).mkString("")
-  }
+  def renderMessages(messages: Array[Msg]): String =
+    new Messages(messages).render()
 
   def printMessages(messages: Array[Msg]): Unit =
      println(renderMessages(messages))
