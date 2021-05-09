2d1b414fbd8c7bce7c22b3f84159e81b14c8de05
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-05-09 13:02:11 +0300

    Include fixed loan costs into calculation of the total loan ínterest rate and costs

--- TypeScript/mortgage.real.rate.calculator.ts
+++ TypeScript/mortgage.real.rate.calculator.ts
@@ -1,13 +1,14 @@
+const monthsInOneYear = 12;
+
 // Basic amortization formula https://mortgagecalculator.mes.fm/amortization-formula-proof
 function getMonthlyPayment(loanValue: number, yearlyRate: number, years: number): number {
-  const totalMonths = years * 12;
-  const monthlyRate = yearlyRate / 12;
+  const totalMonths = years * monthsInOneYear;
+  const monthlyRate = yearlyRate / monthsInOneYear;
   return loanValue * monthlyRate * Math.pow((1 + monthlyRate), totalMonths) / (Math.pow((1 + monthlyRate), totalMonths) - 1);
 }
 
-function getTotalPayment(loanValue: number, yearlyRate: number, years: number): number {
-  const monthlyPayment = getMonthlyPayment(loanValue, yearlyRate, years);
-  return monthlyPayment * 12 * years;
+function getTotalPayment(realMonthlyCosts: number, years: number): number {
+  return realMonthlyCosts * monthsInOneYear * years;
 }
 
 const highestConsideredRatePercent = 100;
@@ -21,15 +22,16 @@ const possibleRates = [...Array<number>(numberOfConsideredRates)].map((_, idx) =
  * Inputs to compute the real interest rate for the suggested payment and the payment + extra costs.
  */
 
-const loanValue = 250000;
+const loanValue = 180000;
 const yearlyRate = 0.005;
 const years = 20;
 
-const suggestedMonthlyPayment = 1200;
+const suggestedMonthlyPayment = 830;
 // Insurance, protection from rising rates, etc.
 const extraMonthlyCosts = 100;
+const extraFixedLoanCosts = 5000;
 
-const realMonthlyCosts = suggestedMonthlyPayment + extraMonthlyCosts;
+const monthlyCostsWithExtraCosts = suggestedMonthlyPayment + extraMonthlyCosts;
 
 const tableOfRates = possibleRates.reduce((table: {[key: string]: number}, rate) => {
   const monthlyPayment = getMonthlyPayment(loanValue, rate, years);
@@ -38,25 +40,34 @@ const tableOfRates = possibleRates.reduce((table: {[key: string]: number}, rate)
 }, {});
 const sortedMonthlyPaymentKeys = Object.keys(tableOfRates).map(key => parseInt(key, 10)).sort((x, y) => x - y);
 
-function estimateRealRate(monthlyPayment: number): number {
-  const nearestComputedMonthlyPaymentIndex = sortedMonthlyPaymentKeys.findIndex(payment => payment > monthlyPayment) - 1;
+function estimateRealRate(monthlyPayment: number, years: number, fixedCosts: number = 0): number {
+  const fixedCostsMonthly = fixedCosts / (years * monthsInOneYear);
+  const fullMonthlyPayment = monthlyPayment + fixedCostsMonthly;
+  const nearestComputedMonthlyPaymentIndex = sortedMonthlyPaymentKeys.findIndex(payment => payment > fullMonthlyPayment) - 1;
   return tableOfRates[sortedMonthlyPaymentKeys[nearestComputedMonthlyPaymentIndex]];
 }
 
-console.log(`Loan value = ${loanValue}`);
-console.log(`Yearly interest rate  = ${yearlyRate}`);
-console.log(`Suggested monthly payment  = ${suggestedMonthlyPayment}`);
-console.log(`Extra monthly costs  = ${extraMonthlyCosts}`);
+console.log(`Loan value =\n\n ${loanValue}\n`);
+console.log(`Yearly interest rate =\n\n ${yearlyRate}\n`);
+console.log(`Suggested monthly payment =\n\n ${suggestedMonthlyPayment}\n`);
+console.log(`Extra monthly costs =\n\n ${extraMonthlyCosts}\n`);
+console.log(`Loan fixed costs =\n\n ${extraFixedLoanCosts}\n`);
 
 console.log('\n');
 
 console.log('Then real loan interest rate and monthly payment are:\n');
 
+const totalPayment = getTotalPayment(monthlyCostsWithExtraCosts, years) + extraFixedLoanCosts;
+console.log(`Loan total value paid back =\n\n ${totalPayment}\n`)
+
 const pureMonthlyPayment = getMonthlyPayment(loanValue, yearlyRate, years);
-console.log(`Pure monthly payment at the nominal loan interest rate of ${yearlyRate * 100} % = ${pureMonthlyPayment}`);
+console.log(`Monthly payment at the nominal loan interest rate of ${yearlyRate * 100} % =\n\n ${pureMonthlyPayment}\n`);
 
-const realYearlyRate = estimateRealRate(suggestedMonthlyPayment);
-console.log(`Real loan yearly rate with monthly payment of ${suggestedMonthlyPayment} = ${realYearlyRate} %`);
+const realYearlyRate = estimateRealRate(suggestedMonthlyPayment, years);
+console.log(`Yearly rate with suggested monthly payment of ${suggestedMonthlyPayment} =\n\n ${realYearlyRate} %\n`);
 
-const realFullYearlyRate = estimateRealRate(realMonthlyCosts);
-console.log(`Full real loan yearly rate with monthly payment of ${realMonthlyCosts} = ${realFullYearlyRate} %`);
+const realYearlyRateExtraMonthlyCosts = estimateRealRate(monthlyCostsWithExtraCosts, years);
+console.log(`Yearly rate with suggested monthly payment of ${suggestedMonthlyPayment}\n and extra monthly payments of ${extraMonthlyCosts} =\n\n ${realYearlyRateExtraMonthlyCosts} %\n`);
+
+const fullYearlyRate = estimateRealRate(monthlyCostsWithExtraCosts, years, extraFixedLoanCosts);
+console.log(`Yearly rate with suggested monthly payment of ${suggestedMonthlyPayment}\n and extra monthly payments of ${extraMonthlyCosts}\n and fixed costs of ${extraFixedLoanCosts} =\n\n ${fullYearlyRate} %\n`);
