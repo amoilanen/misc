5fe8df7acdb4358869ae68047c0b1b5eb42a232f
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-05-18 23:57:07 +0300

    Implement keysMatchingPrefix

--- Scala/handsonscala-excercises/src/main/scala/ch6/Trie.scala
+++ Scala/handsonscala-excercises/src/main/scala/ch6/Trie.scala
@@ -4,24 +4,38 @@ import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
 
 case class TrieNode(value: Char, children: Set[TrieNode]) {
   val isLeaf = children.isEmpty
+
+  def keys(): Set[String] = {
+    if (isLeaf)
+      Set(value.toString)
+    else
+      children.flatMap(child =>
+        child.keys().map(value + _)
+      )
+  }
 }
 
 case class Trie(root: TrieNode) {
 
-  private def findNode(key: String): Option[TrieNode] =
-    if (key.isEmpty)
+  private def findNode(keyPrefix: String): Option[TrieNode] =
+    if (keyPrefix.isEmpty)
       None
     else
-      key.foldLeft(Option(root))((subTrie, nextKeyChar) => {
+      keyPrefix.foldLeft(Option(root))((subTrie, nextKeyChar) => {
         subTrie.flatMap(_.children.find(_.value == nextKeyChar))
       })
 
   def contains(key: String): Boolean =
     findNode(key).map(_.isLeaf).getOrElse(false)
 
-  def prefixesMatchingKey(key: String): Set[String] = ???
+  def prefixesMatchingKey(key: String): Set[String] =
+    ???
 
-  def keysMatchingPrefix(prefix: String): Set[String] = ???
+  def keysMatchingPrefix(prefix: String): Set[String] =
+    findNode(prefix)
+      .map(_.keys())
+      .getOrElse(Set())
+      .map(prefix + _.drop(1))
 }
 
 object Trie extends App {
@@ -61,4 +75,6 @@ object Trie extends App {
   println(trie.contains("ab"))
   println(trie.contains("fgh"))
   println(trie.contains(""))
+
+  println(trie.keysMatchingPrefix("ab"))
 }
