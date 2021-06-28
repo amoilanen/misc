fc34089364c4525c5f63b02e15595ca20fc0f023
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-06-29 00:11:50 +0300

    Re-factor to the function version of traversal without vars

--- Scala/handsonscala-excercises/src/main/scala/ch6/ShortestPath.scala
+++ Scala/handsonscala-excercises/src/main/scala/ch6/ShortestPath.scala
@@ -1,32 +1,62 @@
 package ch6
 
+import scala.annotation.tailrec
+
 object ShortestPath {
 
-  def depthSearchPaths[T](start: T, graph: Map[T, Seq[(T, Int)]]): Map[T, List[T]] = {
-    val shortestLengths = collection.mutable.Map(start -> 0)
-    val shortestPathes = collection.mutable.Map(start -> List(start))
-    val visited = collection.mutable.Set(start)
-    val queue = collection.mutable.ArrayDeque(start)
-    while (queue.nonEmpty) {
-      val current = queue.removeLast()
+  private case class TraversalState[T](
+    queue: List[T],
+    visited: Set[T],
+    shortestLengths: Map[T, Int],
+    shortestPaths: Map[T, List[T]]
+  )
+
+  @tailrec
+  private def traverseNext[T](graph: Map[T, Seq[(T, Int)]], state: TraversalState[T]): TraversalState[T] = {
+    val TraversalState(queue, visited, shortestLengths, shortestPaths) = state
+    if (queue.isEmpty)
+      state
+    else {
+      val current = queue.last
       val currentPathLength = shortestLengths(current)
-      val currentPath = shortestPathes(current)
-      graph(current).foreach({ case (child, edgeLength) =>
-        if (!shortestLengths.get(child).exists(_ <= currentPathLength + edgeLength)) {
-          shortestLengths(child) = currentPathLength + edgeLength
-          shortestPathes(child) = currentPath ++ List(child)
-        }
-        if (!visited(child)) {
-          queue.append(child)
-        }
-        visited.add(current)
+      val currentPath = shortestPaths(current)
+
+      val updatedVisited = visited + current
+
+      val notVisitedChildren = graph(current).filter({
+        case (child, _) => !visited(child)
+      }).map(_._1).toList
+      val updatedQueue = queue.dropRight(1) ++ notVisitedChildren
+
+      val (updatedShortestLengths, updatedShortestPaths) = graph(current).foldLeft((shortestLengths, shortestPaths))({ case (acc, (child, edgeLength)) =>
+        val (shortestLengths, shortestPaths) = acc
+        val foundShorterPath = !shortestLengths.get(child).exists(_ <= currentPathLength + edgeLength)
+        val updatedShortestLengths = if (foundShorterPath)
+          shortestLengths + (child -> (currentPathLength + edgeLength))
+        else
+          shortestLengths
+        val updatedShortestPaths = if (foundShorterPath)
+          shortestPaths + (child -> (currentPath ++ List(child)))
+        else
+          shortestPaths
+        (updatedShortestLengths, updatedShortestPaths)
       })
+
+      traverseNext(graph, TraversalState(updatedQueue, updatedVisited, updatedShortestLengths, updatedShortestPaths))
     }
-    shortestPathes.toMap
+  }
+
+  def depthSearchPaths[T](start: T, graph: Map[T, Seq[(T, Int)]]): Map[T, List[T]] = {
+    val shortestLengths = Map(start -> 0)
+    val shortestPaths = Map(start -> List(start))
+    val visited = Set(start)
+    val queue = List(start)
+    val traversalResult = traverseNext(graph, TraversalState(queue, visited, shortestLengths, shortestPaths))
+    traversalResult.shortestPaths
   }
 
   def shortestPath[T](start: T, dest: T, graph: Map[T, Seq[(T, Int)]]): Seq[T] = {
-    val shortestPathes = depthSearchPaths(start, graph)
-    shortestPathes(dest)
+    val shortestPaths = depthSearchPaths(start, graph)
+    shortestPaths(dest)
   }
 }
