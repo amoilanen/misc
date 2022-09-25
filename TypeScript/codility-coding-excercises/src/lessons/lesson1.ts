77579384e10badce5fcc94dbe49626023dc9c9da
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-09-25 23:51:44 +0300

    Fix linting error

--- TypeScript/codility-coding-excercises/src/lessons/lesson1.ts
+++ TypeScript/codility-coding-excercises/src/lessons/lesson1.ts
@@ -7,18 +7,15 @@ function toBinary(x: number): string {
 //15
 //32
 
-let input =  1041;
+const input =  1041;
 
-let binaryRepresentation = toBinary(input);
-
-console.log(binaryRepresentation);
-
-let sequencesOfZeros = binaryRepresentation.split('1');
+const binaryRepresentation = toBinary(input);
+const sequencesOfZeros = binaryRepresentation.split('1');
 sequencesOfZeros.splice(-1, 1);
 sequencesOfZeros.splice(0, 1);
-let gaps = sequencesOfZeros.filter(sequence => sequence.length > 0)
+const gaps = sequencesOfZeros.filter(sequence => sequence.length > 0)
 
-let gapLengthes = gaps.map(gap => gap.length);
-let longestGap = Math.max.apply(Math, gapLengthes.concat(0));
+const gapLengthes = gaps.map(gap => gap.length);
+const longestGap = Math.max(...gapLengthes.concat(0));
 
 console.log(longestGap);
