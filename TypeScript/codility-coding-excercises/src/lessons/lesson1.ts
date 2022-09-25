1f1fa74c97399381f8d9fd34d2bda94bd23dbd77
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-09-25 23:50:33 +0300

    Add initial code for lesson 1

--- TypeScript/codility-coding-excercises/src/lessons/lesson1.ts
+++ TypeScript/codility-coding-excercises/src/lessons/lesson1.ts
@@ -1,5 +1,24 @@
-let x = 1;
+function toBinary(x: number): string {
+  return x.toString(2);
+}
 
-x = x + 2;
+//Example inputs
+//1041
+//15
+//32
 
-console.log(x);
+let input =  1041;
+
+let binaryRepresentation = toBinary(input);
+
+console.log(binaryRepresentation);
+
+let sequencesOfZeros = binaryRepresentation.split('1');
+sequencesOfZeros.splice(-1, 1);
+sequencesOfZeros.splice(0, 1);
+let gaps = sequencesOfZeros.filter(sequence => sequence.length > 0)
+
+let gapLengthes = gaps.map(gap => gap.length);
+let longestGap = Math.max.apply(Math, gapLengthes.concat(0));
+
+console.log(longestGap);
