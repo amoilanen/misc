defba73e1c31e821d7a477ead5ac79f196b37848
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-09-26 17:28:27 +0300

    Add first test for lesson1

---
+++ TypeScript/codility-coding-excercises/spec/lessons/lesson1.spec.ts
@@ -0,0 +1,5 @@
+import { longestGap } from '../../src/lessons/lesson1';
+
+test('longest gap in 1041', () => {
+  expect(longestGap(1041)).toBe(5);
+});
--- TypeScript/codility-coding-excercises/src/lessons/lesson1.ts
+++ TypeScript/codility-coding-excercises/src/lessons/lesson1.ts
@@ -2,20 +2,18 @@ function toBinary(x: number): string {
   return x.toString(2);
 }
 
+export function longestGap(input: number): number {
+  const binaryRepresentation = toBinary(input);
+  const sequencesOfZeros = binaryRepresentation.split('1');
+  sequencesOfZeros.splice(-1, 1);
+  sequencesOfZeros.splice(0, 1);
+  const gaps = sequencesOfZeros.filter(sequence => sequence.length > 0)
+  
+  const gapLengthes = gaps.map(gap => gap.length);
+  return Math.max(...gapLengthes.concat(0));
+}
+
 //Example inputs
 //1041
 //15
 //32
-
-const input =  1041;
-
-const binaryRepresentation = toBinary(input);
-const sequencesOfZeros = binaryRepresentation.split('1');
-sequencesOfZeros.splice(-1, 1);
-sequencesOfZeros.splice(0, 1);
-const gaps = sequencesOfZeros.filter(sequence => sequence.length > 0)
-
-const gapLengthes = gaps.map(gap => gap.length);
-const longestGap = Math.max(...gapLengthes.concat(0));
-
-console.log(longestGap);
