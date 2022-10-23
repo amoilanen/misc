4e4208280d047cbc5d18923c537fd566c0ae6903
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-10-23 23:56:16 +0300

    Initial solution for 4.1

---
+++ TypeScript/codility-coding-excercises/src/lessons/lesson4.1.ts
@@ -0,0 +1,17 @@
+//TODO: Implement the same algorithm in a more functional language, for example Scala 3
+//TODO: Carefully check corner cases of riverLength and faillintLeavesInTime
+function earliestMomentToCross(riverLength: number, fallingLeavesInTime: Array<number>): number {
+  let leavesToFall = new Set(Array.from(Array(riverLength).keys()))
+  let currentMoment = 0
+  while (currentMoment < fallingLeavesInTime.length && leavesToFall.size > 0) {
+    leavesToFall.delete(fallingLeavesInTime[currentMoment] - 1)
+    currentMoment++
+  }
+  if (leavesToFall.size == 0) {
+    return currentMoment - 1
+  } else {
+    return -1
+  }
+}
+
+console.log(earliestMomentToCross(5, [1, 3, 1, 4, 2, 3, 5, 4]))
