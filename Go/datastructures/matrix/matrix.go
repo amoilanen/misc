c3135c3365548bb961b6c07bd5d57cdf2b2481bd
Anton Moilanen <anton.a.moilanen@gmail.com>
2023-08-16 23:22:10 +0400

    Optimize the matrix multiplication algorithm using Go routines

--- Go/datastructures/matrix/matrix.go
+++ Go/datastructures/matrix/matrix.go
@@ -1,7 +1,10 @@
 package matrix
 
-import "fmt"
-import "golang.org/x/exp/constraints"
+import (
+	"fmt"
+	"golang.org/x/exp/constraints"
+	"runtime"
+)
 
 type Number interface {
 	constraints.Integer | constraints.Float
@@ -71,24 +74,77 @@ func (matrix *Matrix[T]) Negate() *Matrix[T] {
 	return NewMatrixWithElements(matrix.Width, matrix.Height, resultElements)
 }
 
+type MultiplicationJob[T Number] struct {
+	first *Matrix[T]
+	second *Matrix[T]
+	row int
+	column int
+}
+
+type MultiplicationJobResult[T Number] struct {
+	row int
+	column int
+	result T
+}
+
+func mutliplicationWorker[T Number](jobs <-chan MultiplicationJob[T], jobResults chan<- MultiplicationJobResult[T]) {
+	for job := range jobs {
+		row := job.row
+		column := job.column
+		firstElements := job.first.Elements
+		secondElements := job.second.Elements
+		var result T
+		for i := 0; i < job.first.Width; i++ {
+			result = result + firstElements[row][i] * secondElements[i][column]
+		}
+		jobResults <- MultiplicationJobResult[T]{
+			row: row,
+			column: column,
+			result: result,
+		}
+	}
+}
+
 func (matrix *Matrix[T]) MultiplyBy(other *Matrix[T]) (*Matrix[T], error) {
-	//TODO: Optimize using go routines and a worker pool to construct a new matrix
 	if matrix.Width != other.Height {
 		return nil, &MatrixError{
 			Message: fmt.Sprintf("Incompatible matrix dimensions: this [%d, %d], other [%d, %d]", matrix.Width, matrix.Height, other.Width, other.Height),
 		}
 	} else {
+		defaultBatchSize := 50
+		workerNumber := runtime.NumCPU()
+		numJobs := matrix.Height * other.Width
+		jobs := make(chan MultiplicationJob[T], defaultBatchSize)
+		jobResults := make(chan MultiplicationJobResult[T], defaultBatchSize)
+		for w := 1; w <= workerNumber; w++ {
+			go mutliplicationWorker(jobs, jobResults)
+		}
+
+		/*
+		 * Initialize the results and submit the computation jobs
+		 */
 		resultElements := make([][]T, matrix.Height)
 		for row := 0; row < matrix.Height; row++ {
 			resultElements[row] = make([]T, other.Width)
 			for column := 0; column < other.Width; column++ {
-				var result T
-				for i := 0; i < matrix.Width; i++ {
-					result = result + matrix.Elements[row][i] * other.Elements[i][column]
+				jobs <- MultiplicationJob[T]{
+					first: matrix,
+					second: other,
+					row: row,
+					column: column,
 				}
-				resultElements[row][column] = result
 			}
 		}
+		close(jobs)
+
+		/*
+		 * Gather the computation job results
+		 */
+		for i := 0; i < numJobs; i++ {
+			jobResult := <- jobResults
+			resultElements[jobResult.row][jobResult.column] = jobResult.result
+		}
+		close(jobResults)
 		return NewMatrixWithElements(other.Width, matrix.Height, resultElements), nil
 	}
 }
