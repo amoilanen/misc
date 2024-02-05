d52ace5a394f303b47b3dbc5eb7ef879e916e8e0
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-02-05 16:20:31 +0300

    Add naive string search implementation

--- Rust/string-search/src/search.rs
+++ Rust/string-search/src/search.rs
@@ -1,13 +1,30 @@
 
 #[derive(Debug)]
 #[derive(PartialEq)]
-pub struct PatternOccurance {
+pub struct PatternOccurrence {
     pub start: usize,
     pub end: usize
 }
 
-pub fn find_occurances(str: &str, pattern: &str) -> Vec<PatternOccurance> {
-    //TODO: Implement
-    let occurrences = Vec::new();
-    return occurrences
+fn has_match_at_index(index: usize, str: &str, pattern: &str) -> bool {
+    let pattern_length: usize = pattern.len();
+    if index + pattern_length > str.len() {
+        false
+    } else {
+        &str[index..(index + pattern.len())] == pattern
+    }
+}
+
+pub fn find_occurrences(str: &str, pattern: &str) -> Vec<PatternOccurrence> {
+    let mut occurrences: Vec<PatternOccurrence> = Vec::new();
+    let pattern_length: usize = pattern.len();
+    for (index, _) in str.chars().enumerate() {
+        if has_match_at_index(index, str, pattern) {
+            occurrences.push(PatternOccurrence {
+                start: index,
+                end: index + pattern_length
+            })
+        }
+    }
+    return occurrences;
 }
--- Rust/string-search/src/search_tests.rs
+++ Rust/string-search/src/search_tests.rs
@@ -2,12 +2,12 @@
 mod tests {
 
     use crate::search;
-    use crate::search::PatternOccurance;
+    use crate::search::PatternOccurrence;
 
     #[test]
     fn find_single_occurence_in_a_string() {
-        let occurences = search::find_occurances("abcdefghjkl", "def");
-        assert_eq!(occurences, [PatternOccurance {
+        let occurences = search::find_occurrences("abcdefghjkl", "def");
+        assert_eq!(occurences, [PatternOccurrence {
             start: 3,
             end: 6
         }]);
@@ -15,14 +15,14 @@ mod tests {
 
     #[test]
     fn find_multiple_occurences_in_a_string() {
-        let occurences = search::find_occurances("abcdefabcdefabcdef", "cd");
-        assert_eq!(occurences, [PatternOccurance {
+        let occurences = search::find_occurrences("abcdefabcdefabcdef", "cd");
+        assert_eq!(occurences, [PatternOccurrence {
             start: 2,
             end: 4
-        }, PatternOccurance {
+        }, PatternOccurrence {
             start: 8,
             end: 10
-        }, PatternOccurance {
+        }, PatternOccurrence {
             start: 14,
             end: 16
         }]);
@@ -30,28 +30,28 @@ mod tests {
 
     #[test]
     fn no_occurances_found_in_a_string() {
-        let occurences = search::find_occurances("abcdefabcdefabcdef", "ghk");
+        let occurences = search::find_occurrences("abcdefabcdefabcdef", "ghk");
         assert_eq!(occurences, []);
     }
 
     #[test]
     fn no_occurances_found_in_an_empty_string() {
-        let occurences = search::find_occurances("", "ghk");
+        let occurences = search::find_occurrences("", "ghk");
         assert_eq!(occurences, []);
     }
 
     #[test]
     fn multiple_occurances_found_with_an_empty_pattern() {
-        let occurences = search::find_occurances("abc", "");
-        assert_eq!(occurences, [PatternOccurance {
+        let occurences = search::find_occurrences("abc", "");
+        assert_eq!(occurences, [PatternOccurrence {
             start: 0,
-            end: 1
-        }, PatternOccurance {
+            end: 0
+        }, PatternOccurrence {
             start: 1,
-            end: 2
-        }, PatternOccurance {
+            end: 1
+        }, PatternOccurrence {
             start: 2,
-            end: 3
+            end: 2
         }]);
     }
 }
