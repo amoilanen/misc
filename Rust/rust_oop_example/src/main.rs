4a56234213d1d09af6f28ab0fb07e589e70892d1
Anton Moilanen <anton.a.moilanen@gmail.com>
2024-12-31 23:41:30 +0300

    Add plan for further changes

--- Rust/rust_oop_example/src/main.rs
+++ Rust/rust_oop_example/src/main.rs
@@ -35,11 +35,13 @@ impl Post {
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
     fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn name(&self) -> String;
     fn content<'a>(&self, post: &'a Post) -> &'a str {
         ""
     }
 }
 
+#[derive(Debug, PartialEq)]
 struct Draft {}
 
 impl State for Draft {
@@ -49,8 +51,12 @@ impl State for Draft {
     fn approve(self: Box<Self>) -> Box<dyn State> {
         self
     }
+    fn name(&self) -> String {
+        "draft".to_owned()
+    }
 }
 
+#[derive(Debug, PartialEq)]
 struct PendingReview {}
 
 impl State for PendingReview {
@@ -60,7 +66,13 @@ impl State for PendingReview {
     fn approve(self: Box<Self>) -> Box<dyn State> {
         Box::new(Published {})
     }
+    fn name(&self) -> String {
+        "pending_review".to_owned()
+    }
 }
+
+#[derive(Debug, PartialEq)]
+struct Published {}
 
 impl State for Published {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
@@ -72,29 +84,59 @@ impl State for Published {
     fn content<'a>(&self, post: &'a Post) -> &'a str {
         &post.content
     }
+    fn name(&self) -> String {
+        "published".to_owned()
+    }
 }
-
-struct Published {}
 
 fn main() {
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::Post;
 
     #[test]
-    fn should_create_post() {
+    fn should_allow_to_request_review_and_approve_post() {
         let mut post = Post::new();
 
         post.add_text("I ate a salad for lunch today");
         assert_eq!("", post.content());
+        assert_eq!("draft", post.state.as_ref().unwrap().name());
 
         post.request_review();
         assert_eq!("", post.content());
+        assert_eq!("pending_review", post.state.as_ref().unwrap().name());
 
         post.approve();
         assert_eq!("I ate a salad for lunch today", post.content());
+        assert_eq!("published", post.state.as_ref().unwrap().name());
     }
+
+    #[test]
+    fn method_reject_changes_post_state_back_to_draft() {
+        //TODO: Implement
+    }
+
+    #[test]
+    fn requires_two_calls_to_approve_to_transition_to_published_state() {
+        //TODO: Implement
+    }
+
+    #[test]
+    fn text_content_can_be_changed_in_draft_state() {
+        //TODO: Implement
+    }
+
+    #[test]
+    fn text_content_cannot_be_changed_in_pending_review_state() {
+        //TODO: Implement
+    }
+
+    #[test]
+    fn text_content_cannot_be_changed_in_published_state() {
+        //TODO: Implement
+    }
+
+    //TODO: Trying to change the state of the post to an invalid state should result in an error
 }
